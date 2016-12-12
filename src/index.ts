5affb28d28393ccb7deec61d7627831440a6c0ee
Walter Leinert <walter.leinert@outlook.com>
2016-12-12 13:47:39 +0000

    fix für index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 require("reflect-metadata");
 
 export * from './base';
+export * from './diagnostics';
 export * from "./model/decorator";
 export * from "./model/error";
 export * from "./model/metadata";
