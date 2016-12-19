5c80ddd7fb26dcedf0306249ead86fa278bf6319
Walter Leinert <walter.leinert@outlook.com>
2016-12-19 16:30:19 +0000

    RoleController

---
+++ src/ts-express-decorators/controllers/passport/roleController.ts
@@ -0,0 +1,50 @@
+import {
+    Controller, Get, Put, Delete,
+    PathParams, Request,
+    Authenticated
+} from 'ts-express-decorators';
+
+// Fluxgate
+import { ControllerBase } from '../../controllerBase';
+
+import { Role, IRole } from '@fluxgate/common';
+import { RoleService } from '../../services/role.service';
+
+
+@Controller('/role')
+export class RoleController extends ControllerBase<Role, number> {
+    constructor(service: RoleService) {
+        super(service, 'artikel', 'artikel_id');
+    }
+
+    // @Authenticated()
+    @Get('/')
+    public find(
+        ): Promise<Role[]> {
+        return super.findInternal();
+    }
+
+    // @Authenticated()
+    @Get('/:id')
+    public findById(
+        @PathParams('id') id: number
+        ): Promise<Role> {
+        return super.findByIdInternal(id);
+    }
+
+    @Authenticated()
+    @Put('/')
+    public update(
+        @Request() request: Express.Request
+        ): Promise<Role> {
+        throw new Error(`not supported`);
+    }
+
+    @Authenticated({ role: 'admin' })
+    @Delete('/:id')
+    public delete(
+        @PathParams('id') id: number
+        ): Promise<number> {
+        throw new Error(`not supported`);
+    }
+}
