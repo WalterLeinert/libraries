04ec1650b8e507a85e6c0955c49382c2a99779fe
Walter Leinert <walter.leinert@outlook.com>
2016-12-27 16:38:15 +0000

    Umstellung auf webpack, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var mocha = require('gulp-mocha');
+/**
+ * Gulp Buildfile für den Server
+ */
+
+const gulp = require('gulp');
+const del = require('del');
+const typescript = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const mocha = require('gulp-mocha');
+const tscConfig = require('./tsconfig.compile.json');
+
 // var jsdoc = require("gulp-jsdoc");
 
 // Tests
@@ -9,11 +17,35 @@ var mocha = require('gulp-mocha');
 //         .pipe(jsdoc('./documentation-output'))
 // })
 
+gulp.task('really-clean', ['clean'], function (cb) {
+    return del('node_modules');
+})
 
-gulp.task('build', function() {/*SNIP*/});
+// clean the contents of the distribution directory
+gulp.task('clean', function () {
+  return del(['dist', 'build', 'lib', 'dts']);
+})
+
+/**
+ * kompiliert den Server
+ */
+gulp.task('compile', function () {
+  var tsResult = gulp
+    .src('src/**/*.ts')
+    .pipe(sourcemaps.init()) // This means sourcemaps will be generated
+    .pipe(typescript(tscConfig.compilerOptions));
+
+  return tsResult.js
+    .pipe(sourcemaps.write('.', {
+      sourceRoot: ".",
+      includeContent: true
+    }))
+    .pipe(gulp.dest('lib'));
+})
+
 
 //optional - use a tsconfig file
-var tsProject = ts.createProject('./tsconfig.json');
+var tsProject = typescript.createProject('./tsconfig.json');
 gulp.task('test', function() {
     //find test code - note use of 'base'
     return gulp.src('./test/**/*.spec.ts', { base: '.' })
@@ -27,4 +59,4 @@ gulp.task('test', function() {
     }));
 });
 /* single command to hook into VS Code */
-gulp.task('default', ['build','test']);
+gulp.task('default', ['compile', 'test']);
