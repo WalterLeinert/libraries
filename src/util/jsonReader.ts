2cb007c9fe46cae09c9d8af78e30d6e0f98903bf
Walter Leinert <walter.leinert@outlook.com>
2016-12-27 16:37:51 +0000

    Fixes für Import, lokale Variable

--- src/util/jsonReader.ts
+++ src/util/jsonReader.ts
@@ -19,10 +19,9 @@ export class JsonReader {
       * Liest eine Json-Datei @param{jsonPath} und liefert das entsprechede Json-Objekt als @see{T} im Callback @see{cb}
       */
     public static readJson<T>(jsonPath: string, cb) {
-        let fs = require('fs');
-        let data = fs.readFile(jsonPath, (err, data) => {
+        fs.readFile(jsonPath, (err, data) => {
             try {
-                let config = <T>JSON.parse(data);
+                let config = <T>JSON.parse(data.toString());
                 cb(null, config);
             } catch (err) {
                 console.error('Die Json-Konfiguration ${jsonPath} ist kein gültiges JSON-Format.')
