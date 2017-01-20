79f183d76bd32b0cd59c6315fe87dc9b14f432ea
Walter Leinert <walter.leinert@outlook.com>
2017-01-20 16:54:22 +0000

    Dropdown-Attribute, Kommentare

--- src/angular/modules/dropdown-selector/dropdown-selector.component.ts
+++ src/angular/modules/dropdown-selector/dropdown-selector.component.ts
@@ -3,10 +3,23 @@ import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angu
 
 import { IDropdownAdapter } from '../../common/adapter';
 
+
+/**
+ * Fluxgate Dropdown-Komponente
+ * 
+ * Kapselt die Dropdown-Konmponente von PrimeNG.
+ * 
+ * @export
+ * @class DropdownSelectorComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'flx-dropdown-selector',
   template: `
-<p-dropdown [options]="dropdownAdapter.data" [(ngModel)]="selectedValue" (onChange)="onSelectionChanged($event.value)"></p-dropdown>
+<p-dropdown [options]="dropdownAdapter.data" [autoWidth]="autoWidth" [style]="style" [(ngModel)]="selectedValue" 
+    (onChange)="onSelectionChanged($event.value)">
+</p-dropdown>
+
 <div *ngIf="debug">
   <p>Selected Item: {{selectedValue | json}}</p>
 </div>
@@ -14,15 +27,100 @@ import { IDropdownAdapter } from '../../common/adapter';
   styles: []
 })
 export class DropdownSelectorComponent implements OnInit {
+
+  /**
+   * falls true, wird beim Control eine Testausgabe eingeblendet
+   * 
+   * @type {boolean}
+   * @memberOf DropdownSelectorComponent
+   */
   @Input() debug: boolean = true;     // TODO: wenn implementierung fertig auf false setzen
-  @Input() allowNoSelectionText: string = 'Auswahl';
+
+  /**
+   * falls true, wird ein künstlicher erster Eintrag mit dem Text des Members 
+   * allowNoSelectionText erzeugt   
+   * 
+   * @type {boolean}
+   * @memberOf DropdownSelectorComponent
+   */
   @Input() allowNoSelection: boolean = false;
+
+ /**
+   * falls true, wird dieser Text als ein künstlicher erster Eintrag verwendet
+   * 
+   * @type {string}
+   * @memberOf DropdownSelectorComponent
+   */
+  @Input() allowNoSelectionText: string = 'Auswahl';
+
+  /**
+   * setzt das autoWidth-Attribut von p-dropdown
+   * 
+   * @type {boolean}
+   * @memberOf DropdownSelectorComponent
+   */
+  @Input() autoWidth: boolean = true;
+
+
+  /**
+   * * setzt das style-Attribut von p-dropdown
+   * 
+   * @type {string}
+   * @memberOf DropdownSelectorComponent
+   */
+  @Input() style: string;
+
+
+  /**
+   * setzt den Index des zu selektierenden Eintrags (erster Eintrag: 0)
+   * 
+   * @type {number}
+   * @memberOf DropdownSelectorComponent
+   */
   @Input() selectedIndex: number = -1;
+
+
+  /**
+   * der zugehörige Adapter für die Anbindung der Daten für die Werteliste
+   * 
+   * @type {IDropdownAdapter}
+   * @memberOf DropdownSelectorComponent
+   */
   @Input() dropdownAdapter: IDropdownAdapter;
 
+
+  /**
+   * Die Property in der angebundenen Werteliste, welche in der Dropbox angezeigt werden soll
+   * 
+   * @type {string}
+   * @memberOf DropdownSelectorComponent
+   */
   @Input() textField: string = 'text';
+
+  /**
+   * Die Property in der angebundenen Werteliste, welche nach Auswahl 
+   * als 'selectedValue' übernommen werden soll.
+   * 
+   * @type {string}
+   * @memberOf DropdownSelectorComponent
+   */
   @Input() valueField: string = 'value';
+
+
+  /**
+   * der selectionChanged-Event: Event-Parameter ist der selectedValue.
+   * 
+   * @memberOf DropdownSelectorComponent
+   */
   @Output() selectionChanged = new EventEmitter<any>();
+
+
+  /**
+   * Der ausgewählte Wert.
+   * 
+   * @type {*}
+   * @memberOf DropdownSelectorComponent
+   */
   @Output() selectedValue: any = {};
 
   constructor() {
