cea13acd77e30c2fa4669eae93d478f9fb0b7319
Walter Leinert <walter.leinert@outlook.com>
2017-01-26 09:46:19 +0000

    Neu: UserController

---
+++ src/ts-express-decorators-flx/controllers/passport/userController.ts
@@ -0,0 +1,50 @@
+import {
+    Controller, Get, Put, Delete,
+    PathParams, Request,
+    Authenticated
+} from 'ts-express-decorators';
+
+// Fluxgate
+import { ControllerBase } from '../controllerBase';
+
+import { User } from '@fluxgate/common';
+import { UserService } from '../../services/user.service';
+
+
+@Controller('/user')
+export class UserController extends ControllerBase<User, number> {
+    constructor(service: UserService) {
+        super(service, 'user', 'user_id');
+    }
+
+    // @Authenticated()
+    @Get('/')
+    public find(
+        ): Promise<User[]> {
+        return super.findInternal();
+    }
+
+    // @Authenticated()
+    @Get('/:id')
+    public findById(
+        @PathParams('id') id: number
+        ): Promise<User> {
+        return super.findByIdInternal(id);
+    }
+
+    @Authenticated()
+    @Put('/')
+    public update(
+        @Request() request: Express.Request
+        ): Promise<User> {
+        throw new Error(`currently not supported`);
+    }
+
+    @Authenticated({ role: 'admin' })
+    @Delete('/:id')
+    public delete(
+        @PathParams('id') id: number
+        ): Promise<number> {
+        throw new Error(`currently not supported`);
+    }
+}
