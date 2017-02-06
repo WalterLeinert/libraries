93c88daef0076d3d84f50d24eb0a365ed3e1d5f2
Walter Leinert <walter.leinert@outlook.com>
2017-02-06 13:45:25 +0000

    ncu -a; upgrade webpack;

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,10 @@ module.exports = {
     libraryTarget: "commonjs"
   },
   resolve: {
-    extensions: ['', '.ts', '.js'],
+    extensions: ['.ts', '.js'],
     modules: [
       //'node_modules',
-      //path.join(__dirname, 'src'),
+      path.join(__dirname, 'src'),
     ]
   },
   module: {
@@ -27,7 +27,7 @@ module.exports = {
         include: [
           path.resolve(__dirname, 'src')
         ],
-        loader: 'ts',
+        loader: 'ts-loader',
         exclude: [
           /node_modules/,
           /dts/
@@ -52,6 +52,7 @@ module.exports = {
     'passport-local',
     'reflect-metadata',
     'ts-express-decorators',
-    'ts-httpexceptions'
+    'ts-httpexceptions',
+    'ts-log-debug'
   ],
 }
