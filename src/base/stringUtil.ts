5aa6efe737ff9f4beac9176404b613553b035168
Walter Leinert <walter.leinert@outlook.com>
2017-02-07 11:48:19 +0000

    neu: splice + Tests

--- src/base/stringUtil.ts
+++ src/base/stringUtil.ts
@@ -1,3 +1,6 @@
+import { Assert } from './../util/assert';
+import { StringBuilder } from './stringBuilder';
+
 /**
  * String Utilities
  */
@@ -9,4 +12,42 @@ export class StringUtil {
     public static isNullOrEmpty(text: string) {
         return (!text || text == null || text.length <= 0);
     }
+
+
+
+    /**
+     * Ändert den String @param{text}, indem ein Textbereich entfernt wird und/oder ein 
+     * neuer String eingefügt wird 
+     * 
+     * @static
+     * @param {string} text - der zu modifiziernede String
+     * @param {number} start - Index, ab dem der String @{text} geändert wird
+     * @param {number} delCount - Anzahl der zu entfernenden Zeichen in @param{text} ab @param{start}
+     * @param {string} textToInsert - einzufügender Text
+     * @returns {string} der neue String
+     * 
+     * @memberOf StringUtil
+     */
+    public static splice(text: string, start: number, delCount: number, textToInsert?: string): string {
+        Assert.notNull(text);
+        Assert.that(start >= 0 && start <= text.length);
+        Assert.that(delCount >= 0 && delCount <= text.length);
+        Assert.that(start + delCount <= text.length);
+        // Assert.notNullOrEmpty(textToInsert);
+        const sb = new StringBuilder();
+
+        sb.append(text.slice(0, start));
+        if (textToInsert) {
+            sb.append(textToInsert);
+        }
+
+        if (delCount) {
+            sb.append(text.slice(start + delCount));
+        } else {
+            sb.append(text.slice(start));
+        }
+
+        return sb.toString();
+    }
+
 }
---
+++ test/base/stringUtil.spec.ts
@@ -0,0 +1,82 @@
+// tslint:disable:member-access
+
+import * as chai from 'chai';
+import { expect } from 'chai';
+import { suite, test } from 'mocha-typescript';
+
+
+import { StringUtil } from '../../src/base/stringUtil';
+
+
+
+@suite('StringUtil')
+class StringUtilTest {
+
+
+    @test 'should insert Text at 0'() {
+        const text = 'Hallo';
+        return expect(StringUtil.splice(text, 0, 0, '#')).to.be.equal('#Hallo');
+    }
+
+    @test 'should append Text'() {
+        const text = 'Hallo';
+        return expect(StringUtil.splice(text, text.length, 0, '#')).to.be.equal('Hallo#');
+    }
+
+    @test 'should insert Text at 1'() {
+        const text = 'Hallo';
+        return expect(StringUtil.splice(text, 1, 0, '#')).to.be.equal('H#allo');
+    }
+
+
+    @test 'should insert Text at 0 (remove 2)'() {
+        const text = 'Hallo';
+        return expect(StringUtil.splice(text, 0, 2, '#')).to.be.equal('#llo');
+    }
+
+    @test 'should insert Text at 0 (remove 4)'() {
+        const text = 'Hallo';
+        return expect(StringUtil.splice(text, 1, 4, '#')).to.be.equal('H#');
+    }
+
+    @test 'should insert no Text at 1 (remove none)'() {
+        const text = 'Hallo';
+        return expect(StringUtil.splice(text, 1, 0)).to.be.equal('Hallo');
+    }
+
+    @test 'should remove Text at 1 (remove 2)'() {
+        const text = 'Hallo';
+        return expect(StringUtil.splice(text, 1, 2)).to.be.equal('Hlo');
+    }
+
+
+    /**
+     * Expected Errors
+     */
+    @test 'should throw Error (text)'() {
+        const text = undefined;
+        return expect(() => StringUtil.splice(text, 0, 2, '#')).to.throw(Error);
+    }
+
+    @test 'should throw Error (start: -1)'() {
+        const text = 'Hallo';
+        return expect(() => StringUtil.splice(text, -1, 2, '#')).to.throw(Error);
+    }
+
+    @test 'should throw Error (start: text.length+1)'() {
+        const text = 'Hallo';
+        return expect(() => StringUtil.splice(text, text.length + 1, 2, '#')).to.throw(Error);
+    }
+
+
+    @test 'should throw Error (delCount: -1)'() {
+        const text = 'Hallo';
+        return expect(() => StringUtil.splice(text, 0, -1, '#')).to.throw(Error);
+    }
+
+    @test 'should throw Error (delCount: text.length+1)'() {
+        const text = 'Hallo';
+        return expect(() => StringUtil.splice(text, 0, text.length + 1, '#')).to.throw(Error);
+    }
+
+}
