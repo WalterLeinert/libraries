e477f96f9f88034a1cb2216d111b10656a10b6d2
Walter Leinert <walter.leinert@outlook.com>
2017-02-08 18:34:24 +0000

    Monatsnamen über localeSettings

--- src/angular/modules/month-selector/month-selector.component.ts
+++ src/angular/modules/month-selector/month-selector.component.ts
@@ -4,14 +4,22 @@ import { Router } from '@angular/router';
 
 import { Assert, Types } from '@fluxgate/common';
 
+import { PrimeNgCalendarLocale } from '../../../primeng/calendarLocale';
 import { MetadataService } from '../../services';
 import { SelectorBaseComponent } from '../common/selectorBase.component';
+
+export interface IMonth {
+  id: number;
+  name: string;
+  shortname: string;
+}
+
 
 @Component({
   selector: 'flx-month-selector',
   template: `
 <div>
-  <flx-dropdown-selector [data]="month" [(selectedValue)]="selectedValue"
+  <flx-dropdown-selector [data]="months" [(selectedValue)]="selectedValue"
     [style]="style"
     [debug]="debug" name="monthSelector">
   </flx-dropdown-selector>
@@ -21,16 +29,41 @@ import { SelectorBaseComponent } from '../common/selectorBase.component';
 })
 export class MonthSelectorComponent extends SelectorBaseComponent {
 
-  public month: number[] = [
+  public months: IMonth[] = [
   ];
+
+
+  /**
+   * Die textField-Property: steuert, welche Property in @see{IMonth} angezeigt wird.
+   * 
+   * @type {string}
+   * @memberOf MonthSelectorComponent
+   */
+  @Input() public textField: string = 'name';
+
 
   constructor(router: Router, metadataService: MetadataService, changeDetectorRef: ChangeDetectorRef) {
     super(router, metadataService, changeDetectorRef);
 
-    this.style = '{\'width\':\'70px\'}';
+    this.style = '{\'width\':\'150px\'}';
+  }
+
+
+  public ngOnInit() {
+    super.ngOnInit();
+
+    let localeSettings = PrimeNgCalendarLocale[this.locale];
+    if (Types.isUndefined(localeSettings)) {
+      localeSettings = PrimeNgCalendarLocale.en;
+    }
+
 
     for (let month = 1; month <= 12; month++) {
-      this.month.push(month);
+      this.months.push({
+        id: month,
+        name: localeSettings.monthNames[month],
+        shortname: localeSettings.monthNamesShort[month]
+      });
     }
   }
 
