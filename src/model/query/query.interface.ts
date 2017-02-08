bf0a248e7e99f388028aa7090e1529ad99add634
Walter Leinert <walter.leinert@outlook.com>
2017-02-08 11:43:42 +0000

    Erweiterung für Query-Interface

--- src/model/query/query.interface.ts
+++ src/model/query/query.interface.ts
@@ -4,5 +4,12 @@ import { IAttributeSelector } from './attributeSelector.interface';
  * Interface für Knex-Queries
  */
 export interface IQuery {
-    selector: IAttributeSelector;
+
+    /**
+     * Liste von Attributselektoren: diese werden z.Zt. als UND-verknüpft interpretiert!
+     * 
+     * @type {IAttributeSelector[]}
+     * @memberOf IQuery
+     */
+    selectors: IAttributeSelector[];
 }
