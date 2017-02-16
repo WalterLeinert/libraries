78745f332f6870e6681cf5116b08875b2fe7baa9
Walter Leinert <walter.leinert@outlook.com>
2017-02-16 09:44:58 +0000

    tslint

--- src/base/controlDisplayInfo.ts
+++ src/base/controlDisplayInfo.ts
@@ -3,7 +3,7 @@ import { PipeTransform } from '@angular/core';
 
 import { ControlType } from './../angular/modules/common/controlType';
 import { PipeType } from './../angular/services/pipe.service';
-import { IService } from './../angular/services/service.interface'
+import { IService } from './../angular/services/service.interface';
 
 import { IControlDisplayInfo } from './controlDisplayInfo.interface';
 import { DataType, DataTypes } from './dataType';
@@ -13,61 +13,61 @@ import { TextAlignment, TextAlignments } from './textAlignment';
 
 export class ControlDisplayInfo extends DisplayInfo implements IControlDisplayInfo {
 
-    public static DEFAULT: IControlDisplayInfo = new ControlDisplayInfo(
-        DisplayInfo.CURRENT_ITEM,
-        DisplayInfo.CURRENT_ITEM,
-        undefined,
-        undefined,
-        TextAlignments.LEFT,
-        ControlType.Input
-    );
+  public static DEFAULT: IControlDisplayInfo = new ControlDisplayInfo(
+    DisplayInfo.CURRENT_ITEM,
+    DisplayInfo.CURRENT_ITEM,
+    undefined,
+    undefined,
+    TextAlignments.LEFT,
+    ControlType.Input
+  );
 
 
-    public static isRightAligned(dataType: DataType) {
-        switch (dataType) {
-            case DataTypes.NUMBER:
-            case DataTypes.DATE:
-            case DataTypes.TIME:
-                return true;
-            default:
-                return false;
-        }
+  public static isRightAligned(dataType: DataType) {
+    switch (dataType) {
+      case DataTypes.NUMBER:
+      case DataTypes.DATE:
+      case DataTypes.TIME:
+        return true;
+      default:
+        return false;
     }
+  }
 
 
-    constructor(textField?: string, valueField?: string, dataType?: DataType, private _style?: string,
-        private _textAlignment?: TextAlignment,
-        private _controlType: ControlType = ControlType.Input,
-        private _enumInfo?: IEnumDisplayInfo, private _pipe?: PipeType | PipeTransform,
-        private _pipeArgs?: string, private _pipeLocale?: string) {
-        super(textField, valueField, dataType);
-    }
+  constructor(textField?: string, valueField?: string, dataType?: DataType, private _style?: string,
+    private _textAlignment?: TextAlignment,
+    private _controlType: ControlType = ControlType.Input,
+    private _enumInfo?: IEnumDisplayInfo, private _pipe?: PipeType | PipeTransform,
+    private _pipeArgs?: string, private _pipeLocale?: string) {
+    super(textField, valueField, dataType);
+  }
 
-    public get controlType(): ControlType {
-        return this._controlType;
-    }
+  public get controlType(): ControlType {
+    return this._controlType;
+  }
 
-    public get enumInfo(): IEnumDisplayInfo {
-        return this._enumInfo;
-    }
+  public get enumInfo(): IEnumDisplayInfo {
+    return this._enumInfo;
+  }
 
-    public get style(): string {
-        return this._style;
-    }
+  public get style(): string {
+    return this._style;
+  }
 
-    public get textAlignment(): TextAlignment {
-        return this._textAlignment;
-    }
+  public get textAlignment(): TextAlignment {
+    return this._textAlignment;
+  }
 
-    public get pipe(): PipeType | PipeTransform {
-        return this._pipe;
-    }
+  public get pipe(): PipeType | PipeTransform {
+    return this._pipe;
+  }
 
-    public get pipeArgs(): string {
-        return this._pipeArgs;
-    }
+  public get pipeArgs(): string {
+    return this._pipeArgs;
+  }
 
-    public get pipeLocale(): string {
-        return this._pipeLocale;
-    }
+  public get pipeLocale(): string {
+    return this._pipeLocale;
+  }
 }
