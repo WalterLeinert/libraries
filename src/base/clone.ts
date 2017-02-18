05ee173f2135ac1f3c7bb3b87183d52bdc302efc
Walter Leinert <walter.leinert@outlook.com>
2017-02-18 13:19:12 +0000

    Clone neu + Tests

--- src/base/clone.ts
+++ src/base/clone.ts
@@ -1,17 +1,94 @@
+// tslint:disable-next-line:no-var-requires
+const entries = require('object.entries');
+
+import { Types } from '../types/types';
+import { Assert } from '../util/assert';
+import { ICtor } from './ctor';
+
+
 export class Clone {
 
-    /**
-     * Liefert einen Clone von @param{value} mittels @see{JSON.stringify} und @see{JSON.parse}.
-     * 
-     * @static
-     * @template T
-     * @param {T} value
-     * @returns {T}
-     * 
-     * @memberOf Clone
-     */
-    public static clone<T>(value: T): T {
-        const cloneString = JSON.stringify(value);
-        return JSON.parse(cloneString);
+  /**
+   * Liefert einen Clone von @param{value}
+   * 
+   * @static
+   * @template T
+   * @param {T} value
+   * @returns {T}
+   * 
+   * @memberOf Clone
+   */
+  public static clone<T>(value: T): T {
+    if (value === undefined || value === null) {
+      return value;
     }
+
+    // falls clone-Methode vorhanden ist nehmen wir diese
+    if ((value as any).clone && Types.isFunction((value as any).clone)) {
+      return (value as any).clone();
+    }
+
+    //
+    // wir verwenden entries, da die 'for (const attr in value) { ...' Loop auch getter ohne Setter liefert,
+    // was dann bei der Wertzweisung nicht funktioniert!
+    //
+    const valueEntries = entries(value);
+
+    // neue Instanz erzeugen
+    const clonedObj = new (value.constructor as any)();
+
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < valueEntries.length; i++) {
+      const entry = valueEntries[i];
+      const entryName = entry[0];
+      const entryValue = entry[1];
+
+      if (typeof entryValue === 'object') {
+        // rekursiv clonen
+        clonedObj[entryName] = Clone.clone(entryValue);
+      } else {
+        clonedObj[entryName] = entryValue;
+      }
+    }
+    return clonedObj;
+  }
+
+
+  /**
+   * Verifiziert, dass @param{clonedValue} wirklich ein deep clone von @param{value} ist.
+   */
+  public static verifyClone<T>(value: T, clonedValue: T, attrName?: string) {
+    if (value === clonedValue) {
+      throw new Error(`value identical to clonedValue: attrName = ${attrName}`);
+    }
+
+    const valueEntries = entries(value);
+    const clonedValueEntries = entries(clonedValue);
+    Assert.that(valueEntries.length === clonedValueEntries.length);
+
+
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < valueEntries.length; i++) {
+      const entry = valueEntries[i];
+      const entryName = entry[0];
+      const entryValue = entry[1];
+
+      const clonedEntry = clonedValueEntries[i];
+      const clonedEntryName = clonedEntry[0];
+      const clonedEntryValue = clonedEntry[1];
+
+      Assert.that(entryName === clonedEntryName);
+
+      Assert.that(typeof entryValue === typeof clonedEntryValue);
+
+      if (typeof entryValue === 'object') {
+        if (entryValue === clonedEntryValue) {
+          throw new Error(`value[${entryValue}] identical to clonedValue[${clonedEntryValue}]: attrName = ${attrName}`);
+        }
+
+        Clone.verifyClone(entryValue, clonedEntryValue, entryName);
+      }
+    }
+
+  }
 }
---
+++ test/base/clone.spec.ts
@@ -0,0 +1,119 @@
+// tslint:disable:member-access
+// tslint:disable:max-classes-per-file
+
+import * as chai from 'chai';
+import { expect } from 'chai';
+import { suite, test } from 'mocha-typescript';
+
+
+import { Clone } from '../../src/base/clone';
+
+class Test {
+  constructor(public id: number, private _test: boolean) {
+  }
+
+  public get test(): boolean {
+    return this._test;
+  }
+}
+
+class TestDerived extends Test {  
+  private names: string[] = ['a', 'b', 'c'];
+
+  constructor(public name: string, id: number, private _today: Date, test: boolean = true) {
+    super(id, test);
+    // ok
+  }
+
+  public get today(): Date {
+    return this._today;
+  }
+}
+
+
+@suite('Clone')
+class CloneTest {
+
+  @test 'should check TestDerived properties'() {
+    const test = new TestDerived('Walter', 4711, new Date());
+    expect(test.id).to.equal(4711);
+    expect(test.name).to.equal('Walter');
+  }
+
+
+  @test 'should clone null'() {
+    const test = null;
+    const testCloned = Clone.clone<TestDerived>(test);
+    expect(testCloned).to.deep.equal(test);
+    expect(() => Clone.verifyClone(test, testCloned)).not.to.throw;
+  }
+
+  @test 'should clone undefined'() {
+    const test = null;
+    const testCloned = Clone.clone<TestDerived>(test);
+    expect(testCloned).to.deep.equal(test);
+    expect(() => Clone.verifyClone(test, testCloned)).not.to.throw;
+  }
+
+
+
+  @test 'should test instance identity'() {
+    const test = new TestDerived('Walter', 4711, new Date());
+    expect(test).to.deep.equal(test);
+    expect(test).to.equal(test);
+    expect(test === test).to.be.true;
+  }
+
+
+  @test 'should test instance equality'() {
+    const test1 = new TestDerived('Walter', 4711, new Date());
+    const test2 = new TestDerived('Christian', 4712, new Date());
+    expect(test1).to.not.deep.equal(test2);
+    expect(test1).to.not.equal(test2);
+    expect(test1 === test2).to.be.not.true;
+  }
+
+ 
+  @test 'should clone'() {
+    const test = new TestDerived('Walter', 4711, new Date());
+    const testCloned = Clone.clone<TestDerived>(test);
+    expect(testCloned).to.deep.equal(test);
+
+    expect(test === testCloned).to.be.not.true;
+    expect(() => Clone.verifyClone(test, testCloned)).not.to.throw;
+  }
+
+  @test 'should verifyClone'() {
+    const test = new TestDerived('Walter', 4711, new Date());
+    const testCloned = Clone.clone<TestDerived>(test);
+    expect(() => Clone.verifyClone(test, test)).not.to.throw;
+  }
+
+}
+
+
+class TestWithClone {
+  constructor(public id: number, private _test: boolean) {
+  }
+
+  public get test(): boolean {
+    return this._test;
+  }
+
+  public clone(): TestWithClone {
+    return new TestWithClone(this.id, this._test);
+  }
+}
+
+
+@suite('Clone by clone()')
+class CloneByCloneTest {
+
+  @test 'should clone by clone function'() {
+    const test = new TestWithClone(4711, false);
+    const testCloned = Clone.clone(test);
+    expect(testCloned).to.deep.equal(test);
+    expect(() => Clone.verifyClone(test, testCloned)).not.to.throw;
+  }
+
+}
