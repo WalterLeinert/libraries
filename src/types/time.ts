e965f4c1f6520ec8fcc0bcc13e36707fb8749d89
Walter Leinert <walter.leinert@outlook.com>
2017-02-19 09:45:46 +0000

    Support für "default constructor"

--- src/types/time.ts
+++ src/types/time.ts
@@ -2,7 +2,7 @@ import leftPad = require('left-pad');
 import { StringBuilder } from './../base/stringBuilder';
 import { Assert } from './../util/assert';
 
-import { Hour, ShortTime } from '.';
+import { Hour, ShortTime, Types } from '.';
 
 
 /**
@@ -13,137 +13,137 @@ import { Hour, ShortTime } from '.';
  */
 export class Time extends ShortTime {
 
-    /**
-     * Erzeugt aus dem String @param{text} eine @see{Time}-Instanz.
-     * 
-     * @static
-     * @param {string} text
-     * @returns {Time}
-     * 
-     * @memberOf Time
-     */
-    public static parse(text: string): Time {
-        Assert.notNullOrEmpty(text);
+  /**
+   * Erzeugt aus dem String @param{text} eine @see{Time}-Instanz.
+   * 
+   * @static
+   * @param {string} text
+   * @returns {Time}
+   * 
+   * @memberOf Time
+   */
+  public static parse(text: string): Time {
+    Assert.notNullOrEmpty(text);
 
-        const parts = text.split(':');
-        Assert.that(parts.length === 3);
+    const parts = text.split(':');
+    Assert.that(parts.length === 3);
 
-        const hour = +parts[0];
-        const minute = +parts[1];
-        const second = +parts[2];
+    const hour = +parts[0];
+    const minute = +parts[1];
+    const second = +parts[2];
 
-        Assert.that(hour >= 0 && hour < 24);
+    Assert.that(hour >= 0 && hour < 24);
 
-        return new Time(hour as Hour, minute, second);
+    return new Time(hour as Hour, minute, second);
+  }
+
+
+  /**
+   * Erzeugt eine @see{Time}-Instanz aus dem angegebenen Objekt, falls dieses
+   * die Properties der @see{Time}-Klasse enthält.
+   * 
+   * @static
+   * @param {*} obj
+   * @returns
+   * 
+   * @memberOf Time
+   */
+  public static createFrom(obj: any) {
+    // tslint:disable:no-string-literal
+    const hour: Hour = obj['hour'];
+    const minute: number = obj['minute'];
+    const second: number = obj['second'];
+
+    const sb = new StringBuilder();
+    if (hour === undefined) {
+      sb.appendWithDelimiter('hour');
     }
-
-
-    /**
-     * Erzeugt eine @see{Time}-Instanz aus dem angegebenen Objekt, falls dieses
-     * die Properties der @see{Time}-Klasse enthält.
-     * 
-     * @static
-     * @param {*} obj
-     * @returns
-     * 
-     * @memberOf Time
-     */
-    public static createFrom(obj: any) {
-        // tslint:disable:no-string-literal
-        const hour: Hour = obj['hour'];
-        const minute: number = obj['minute'];
-        const second: number = obj['second'];
-
-        const sb = new StringBuilder();
-        if (hour === undefined) {
-            sb.appendWithDelimiter('hour');
-        }
-        if (minute === undefined) {
-            sb.appendWithDelimiter('minute');
-        }
-        if (second === undefined) {
-            sb.appendWithDelimiter('second');
-        }
-        Assert.that(sb.isEmpty, `Property ${sb} fehlt.`);
-
-        return new Time(hour, minute, second);
+    if (minute === undefined) {
+      sb.appendWithDelimiter('minute');
     }
-
-    /**
-     * Erzeugt aus der Zeitangabe in Sekunden eine @see{Time}-Instanz.
-     * 
-     * @static
-     * @param {number} seconds
-     * @returns {Time}
-     * 
-     * @memberOf Time
-     */
-    public static createFromSeconds(seconds: number): Time {
-        const hour = Math.floor(seconds / 3600);
-        const minute = Math.floor((seconds - hour * 3600) / 60);
-        const second = (seconds - hour * 3600 - minute * 60);
-
-        return new Time(hour as Hour, minute, second);
+    if (second === undefined) {
+      sb.appendWithDelimiter('second');
     }
+    Assert.that(sb.isEmpty, `Property ${sb} fehlt.`);
+
+    return new Time(hour, minute, second);
+  }
+
+  /**
+   * Erzeugt aus der Zeitangabe in Sekunden eine @see{Time}-Instanz.
+   * 
+   * @static
+   * @param {number} seconds
+   * @returns {Time}
+   * 
+   * @memberOf Time
+   */
+  public static createFromSeconds(seconds: number): Time {
+    const hour = Math.floor(seconds / 3600);
+    const minute = Math.floor((seconds - hour * 3600) / 60);
+    const second = (seconds - hour * 3600 - minute * 60);
+
+    return new Time(hour as Hour, minute, second);
+  }
 
 
-    /**
-     * Creates an instance of Time.
-     * 
-     * @param {Hour} _hour
-     * @param {number} _minute
-     * @param {number} _second
-     * 
-     * @memberOf Time
-     */
-    constructor(hour: Hour, minute: number, public second: number) {
-        super(hour, minute);
+  /**
+   * Creates an instance of Time.
+   * 
+   * @param {Hour} _hour
+   * @param {number} _minute
+   * @param {number} _second
+   * 
+   * @memberOf Time
+   */
+  public constructor(hour: Hour, minute: number, public second: number) {
+    super(hour, minute);
 
-        Assert.that(second >= 0 && second <= 59);
-    }
+    Assert.that(!Types.isUndefined(second) && (second >= 0 && second <= 59));
+  }
 
-    /**
-     * Liefert die String-Repräsentation.
-     * 
-     * @returns {string}
-     * 
-     * @memberOf Time
-     */
-    public toString(): string {
-        return `${super.toString()}:${leftPad(this.second, 2, 0)}`;
-    }
+  /**
+   * Liefert die String-Repräsentation.
+   * 
+   * @returns {string}
+   * 
+   * @memberOf Time
+   */
+  public toString(): string {
+    return `${super.toString()}:${leftPad(this.second, 2, 0)}`;
+  }
 
-    public toSeconds(): number {
-        return this.hour * 3600 + this.minute * 60 + this.second;
-    }
+  public toSeconds(): number {
+    return this.hour * 3600 + this.minute * 60 + this.second;
+  }
 
 
-    /**
-     * Addiert zur Zeit der aktuellen Instanz die Zeit @param{time} und liefert
-     * das Ergebnis.
-     * 
-     * @param {Time} time
-     * @returns {Time}
-     * 
-     * @memberOf Time
-     */
-    public add(time: Time): Time {
-        const timeInSeconds = this.toSeconds() + time.toSeconds();
-        return Time.createFromSeconds(timeInSeconds);
-    }
+  /**
+   * Addiert zur Zeit der aktuellen Instanz die Zeit @param{time} und liefert
+   * das Ergebnis.
+   * 
+   * @param {Time} time
+   * @returns {Time}
+   * 
+   * @memberOf Time
+   */
+  public add(time: Time): Time {
+    const timeInSeconds = this.toSeconds() + time.toSeconds();
+    return Time.createFromSeconds(timeInSeconds);
+  }
 
-    /**
-     * Subrahiert von der Zeit der aktuellen Instanz die Zeit @param{time} und liefert
-     * das Ergebnis.
-     * 
-     * @param {Time} time
-     * @returns {Time}
-     * 
-     * @memberOf Time
-     */
-    public subtract(time: Time): Time {
-        const timeInSeconds = this.toSeconds() - time.toSeconds();
-        Assert.that(timeInSeconds >= 0);
-        return Time.createFromSeconds(timeInSeconds);
-    }
+  /**
+   * Subrahiert von der Zeit der aktuellen Instanz die Zeit @param{time} und liefert
+   * das Ergebnis.
+   * 
+   * @param {Time} time
+   * @returns {Time}
+   * 
+   * @memberOf Time
+   */
+  public subtract(time: Time): Time {
+    const timeInSeconds = this.toSeconds() - time.toSeconds();
+    Assert.that(timeInSeconds >= 0);
+    return Time.createFromSeconds(timeInSeconds);
+  }
 }
---
+++ test/types/short-time.spec.ts
@@ -0,0 +1,186 @@
+// tslint:disable:max-classes-per-file
+// tslint:disable:member-access
+
+// require('reflect-metadata');
+
+import * as chai from 'chai';
+import { expect } from 'chai';
+import { suite, test } from 'mocha-typescript';
+
+import { ShortTime, Types } from '../../src/types';
+import { AssertionError } from '../../src/util';
+
+const expedtedTimes = [
+  {
+    time: new ShortTime(0, 0),
+    text: '00:00'
+  },
+  {
+    time: new ShortTime(8, 30),
+    text: '08:30'
+  },
+  {
+    time: new ShortTime(18, 0),
+    text: '18:00'
+  },
+  {
+    time: new ShortTime(23, 59),
+    text: '23:59'
+  }
+];
+
+
+@suite('ShortTime (HH:mm)')
+class ShortTimeTest {
+
+  @test 'should create instance of class ShortTime (default ctor)'() {
+    return expect(Types.construct(ShortTime)).to.be.not.null;
+  }
+
+  @test 'should create instance of class ShortTime'() {
+    return expect(new ShortTime(8, 0)).to.be.not.null;
+  }
+
+  @test 'should format times'() {
+    expedtedTimes.forEach((test) => {
+      expect(test.time.toString()).to.equal(test.text);
+    });
+  }
+
+  @test 'should parse times'() {
+    expedtedTimes.forEach((test) => {
+      expect(ShortTime.parse(test.text)).to.deep.equal(test.time);
+    });
+  }
+
+  @test 'should createFrom obj'() {
+    const timeObj = {
+      hour: 12,
+      minute: 10
+    };
+
+    expect(ShortTime.createFrom(timeObj).hour).to.equal(timeObj.hour);
+    expect(ShortTime.createFrom(timeObj).minute).to.equal(timeObj.minute);
+  }
+
+  @test 'should throw exceptions from createFromj'() {
+    expect(() => ShortTime.createFrom({
+    })).to.throw(Error, 'Property hour, minute fehlt');
+
+    expect(() => ShortTime.createFrom({
+      hour: 12
+    })).to.throw(Error, 'Property minute fehlt.');
+
+    expect(() => ShortTime.createFrom({
+      hour: 12,
+      minute: 15
+    })).to.not.throw;
+  }
+
+  @test 'should allow 00 seconds'() {
+    const time = '12:11';
+    expect(ShortTime.parse(`${time}:00`).toString()).to.be.equal(time);
+  }
+
+  @test 'should not allow ss seconds'() {
+    const time = '12:11:33';
+    expect(() => ShortTime.parse(time))
+      .to.throw(Error, `Zeit ${time}: Falls Sekunden angegeben sind, darf der Wert nur 00 sein.`);
+  }
+
+
+
+  @test 'should throw an exception for 60 minutes '() {
+    return expect(() => new ShortTime(8, 60)).to.throw(Error);
+  }
+
+  @test 'should throw an exception for -1 minutes'() {
+    return expect(() => new ShortTime(8, -1)).to.throw(Error);
+  }
+
+}
+
+
+
+function timeToMinute(time: ShortTime): number {
+  return time.hour * 60 + time.minute;
+}
+
+
+@suite('ShortTime (add, subtract)')
+class ShortTimeOperationsTest {
+
+  @test 'should get minutes from hour'() {
+    const time = new ShortTime(8, 0);
+    return expect(time.toMinutes()).to.equal(timeToMinute(time));
+  }
+
+  @test 'should get minutes from minute'() {
+    const time = new ShortTime(0, 10);
+    return expect(time.toMinutes()).to.equal(timeToMinute(time));
+  }
+
+  @test 'should get hours'() {
+    const time = new ShortTime(0, 10);
+    return expect(time.toHours()).to.be.closeTo(time.toMinutes() / 60, 0.05);
+  }
+
+  @test 'should get hours 2'() {
+    const time = new ShortTime(12, 15);
+    return expect(time.toHours()).to.be.closeTo(time.toMinutes() / 60, 0.05);
+  }
+
+  @test 'should get hours rounded to 2 decimal places'() {
+    const time = new ShortTime(12, 15);
+    return expect(time.toHours(2)).to.equal(12.25);
+  }
+
+  @test 'should get hours rounded to 3 decimal places'() {
+    const time = new ShortTime(12, 11);
+    return expect(time.toHours(3)).to.equal(12.183);
+  }
+
+
+
+  @test 'should get time from minutes'() {
+    const time = new ShortTime(12, 15);
+    return expect(ShortTime.createFromMinutes(time.toMinutes())).to.deep.equal(time);
+  }
+
+  @test 'should get time from seconds -> Exception'() {
+    const time = new ShortTime(23, 59);
+    return expect(() => ShortTime.createFromMinutes(time.toMinutes() + 1)).to.throw(Error);
+  }
+
+  @test 'should add times'() {
+    const time1 = new ShortTime(12, 0);
+    const time2 = new ShortTime(1, 30);
+    const result = time1.add(time2);
+    const expected = new ShortTime(13, 30);
+
+    return expect(result).to.deep.equal(expected);
+  }
+
+  @test 'should subtract times'() {
+    const time1 = new ShortTime(12, 0);
+    const time2 = new ShortTime(1, 30);
+    const result = time1.subtract(time2);
+    const expected = new ShortTime(10, 30);
+
+    return expect(result).to.deep.equal(expected);
+  }
+
+
+  @test 'should add times -> Exception'() {
+    const time1 = new ShortTime(23, 59);
+    const time2 = new ShortTime(1, 30);
+    return expect(() => time1.add(time2)).to.throw(Error);
+  }
+
+  @test 'should subtract times -> Exception'() {
+    const time1 = new ShortTime(0, 0);
+    const time2 = new ShortTime(1, 30);
+    return expect(() => time1.subtract(time2)).to.throw(Error);
+  }
+
+}
