e35311da5baf646879836a07ab9da6fdb9de1ce8
Walter Leinert <walter.leinert@outlook.com>
2017-02-22 21:05:47 +0000

    Beispiel für json Druckvorlage

---
+++ test/reflection/json-format.spec.ts
@@ -0,0 +1,103 @@
+
+import { suite, test } from 'mocha-typescript';
+
+ enum TableType {
+  Master,
+  Detail
+}
+
+
+ interface IField {
+  name: string;
+  type: string;
+}
+
+ interface ITable {
+  name: string;
+  type: TableType;
+  fields: IField[];
+}
+
+ interface IOption {
+  title: string;
+  doctype?: string;
+  printer?: string;
+  color?: string;
+  copies?: string;
+}
+
+
+ interface IData {
+  table: string;
+  records: { [key: string]: any };
+}
+
+ interface IPrintjob {
+  options: IOption[];
+  data: IData[];
+}
+
+ interface IRootObject {
+  report: string;
+  tables: ITable[];
+  printjobs: IPrintjob[];
+}
+
+
+const doc: IRootObject = {
+  report: 'Angebot.lrf',
+
+  tables: [
+    {
+      name: 'Master',
+      type: TableType.Master,
+      fields: [
+        {
+          name: 'id',
+          type: 'number'
+        },
+        {
+          name: 'name',
+          type: 'string'
+        }
+      ]
+    },
+
+  ],
+  printjobs: [
+    {
+
+      options: [
+        {
+          title: 'Ausdruck'
+        }
+      ],
+      data: [
+        {
+          table: 'Details',
+          records: [
+            {
+              id: 1, name: 'hugo'
+            },
+            {
+              id: 2, name: 'hirsch'
+            }
+          ]
+        }
+      ]
+    }
+
+  ]
+
+};
+
+
+
+
+@suite('ReflectionTypeReflector')
+class ReflectionTest {
+
+  @test 'should create instance of TypeReflector'() {
+    console.log(JSON.stringify(doc));
+  }
+}
