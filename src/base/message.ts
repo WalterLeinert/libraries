012ce6be6afa33392486278f7b6e708ac42625ff
Walter Leinert <walter.leinert@outlook.com>
2017-03-12 14:27:38 +0100

    neu: PublisherSubscriber; IMessage jetzt in common

---
+++ src/base/message.ts
@@ -0,0 +1,42 @@
+
+export enum MessageSeverity {
+  Info,
+  Warn,
+  Error,
+  Fatal
+}
+
+
+/**
+ * Interface für allgemeine Messages
+ * 
+ * @export
+ * @interface IMessage
+ */
+export interface IMessage {
+  
+  /**
+   * Severity
+   * 
+   * @type {MessageSeverity}
+   * @memberOf IMessage
+   */
+  severity: MessageSeverity;
+  
+  /**
+   * Summary der Message
+   * 
+   * @type {string}
+   * @memberOf IMessage
+   */
+  summary: string;
+
+
+  /**
+   * Messagedetails
+   * 
+   * @type {string}
+   * @memberOf IMessage
+   */
+  detail?: string;
+}
