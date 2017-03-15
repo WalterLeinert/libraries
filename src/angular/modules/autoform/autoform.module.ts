c28a36d5602e27362cda33b7d089f8a95789d852
Walter Leinert <walter.leinert@outlook.com>
2017-03-15 12:57:39 +0100

    überflüssigen Import enfernt

--- src/angular/modules/autoform/autoform.module.ts
+++ src/angular/modules/autoform/autoform.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // PrimeNG
