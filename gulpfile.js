8fcacfb8074d09d43411521fd8164c91ce23f581
Walter Leinert <walter.leinert@outlook.com>
2017-03-31 11:28:02 +0100

    Fixes für build

--- gulpfile.js
+++ gulpfile.js
@@ -64,13 +64,8 @@ gulp.copy = function (src, dest) {
 /**
  * Common build
  */
-gulp.task('update-fluxgate-common', function (cb) {
-  //execCommand('npm uninstall --save @fluxgate/common', 'common', null, cb);
-  execCommand('npm uninstall --save @fluxgate/common && npm install --save @fluxgate/common', 'common', null, cb);
-})
-
 gulp.task('lint-common', function (cb) {
-  execCommand('gulp lint', 'common', null, cb);
+  execCommand('gulp tslint', 'common', null, cb);
 })
 
 gulp.task('really-clean-common', ['clean-common'], function (cb) {
@@ -85,21 +80,23 @@ gulp.task('build-common', function (cb) {
   execCommand('gulp', 'common', bufferSize, cb);
 })
 
-gulp.task('deploy-common', function (cb) {
-  execCommand('gulp deploy', 'common', null, cb);
+gulp.task('publish-common', function (cb) {
+  execCommand('gulp publish -f', 'common', null, cb);
 })
+
+gulp.task('build-all-common', gulpSequence('clean-common', 'build-common', 'publish-common'))
+
 
 
 /**
  * Server build
  */
 gulp.task('update-fluxgate-server', function (cb) {
-  //execCommand('npm uninstall --save @fluxgate/common @fluxgate/server', 'server', null, cb);
-  execCommand('npm uninstall --save @fluxgate/common @fluxgate/server && npm install --save @fluxgate/common @fluxgate/server', 'server', null, cb);
+ execCommand('gulp update-fluxgate-common', 'server', null, cb);
 })
 
 gulp.task('lint-server', function (cb) {
-  execCommand('gulp lint', 'server', null, cb);
+  execCommand('gulp tslint', 'server', null, cb);
 })
 
 gulp.task('really-clean-server', ['clean-server'], function (cb) {
@@ -114,21 +111,23 @@ gulp.task('build-server', function (cb) {
   execCommand('gulp', 'server', bufferSize, cb);
 })
 
-gulp.task('run-server', function (cb) {
-  execCommand('gulp run', 'server', bufferSize, cb);
+gulp.task('publish-server', function (cb) {
+  execCommand('gulp publish -f', 'server', null, cb);
 })
+
+gulp.task('build-all-server', gulpSequence('clean-server', 'build-server', 'publish-server'))
+
 
 
 /**
  * Client Tasks
  */
 gulp.task('update-fluxgate-client', function (cb) {
-  //execCommand('npm uninstall --save @fluxgate/common @fluxgate/client', 'client', null, cb);
-  execCommand('npm uninstall --save @fluxgate/common @fluxgate/client && npm install --save @fluxgate/common @fluxgate/client', 'client', null, cb);
+ execCommand('gulp update-fluxgate-common', 'client', null, cb);
 })
 
 gulp.task('lint-client', function (cb) {
-  execCommand('gulp lint', 'client', null, cb);
+  execCommand('gulp tslint', 'client', null, cb);
 })
 
 gulp.task('really-clean-client', ['clean-client'], function (cb) {
@@ -143,28 +142,11 @@ gulp.task('build-client', function (cb) {
   execCommand('gulp', 'client', bufferSize, cb);
 })
 
-gulp.task('run-client', function (cb) {
-  execCommand('ng serve', 'client', bufferSize, cb);
+gulp.task('publish-client', function (cb) {
+  execCommand('gulp publish -f', 'client', null, cb);
 })
 
-gulp.task('deploy-client', function (cb) {
-  execCommand('gulp deploy', 'client', null, cb);
-}),
-
-
-
-// clean the contents of the distribution directory
-gulp.task('clean-deploy', function () {
-  return del('dist');
-});
-
-let dist = 'server/dist/server/src/';
-/*
-gulp.task('deploy-server', function () {
-  return gulp
-    .copy('server/dist/server/src/**', 'dist');
-})
-*/
+gulp.task('build-all-client', gulpSequence('clean-client', 'build-client', 'publish-client'))
 
 
 
@@ -194,98 +176,22 @@ gulp.task('info', function () {
 })
 
 
-//-----------------------------------------------------------------------
-// Start: Docker build, up, down
-//-----------------------------------------------------------------------
-
-gulp.task('docker-build', function (cb) {
-  // Hinweis: das Environment spielt für den build keine große Rolle, entsprechende Files
-  // (wie .docker/env/app.development.env) müssen nur existieren
-  //
-  // Bei docker-up wird das Environment sehr wohl ausgewertet und definiert, womit die Container laufen! 
-  const env = 'development'
-
-  let buildEnv = {
-        APP_ENV: env,
-        NODE_ENV: env,
-  }
-  console.log('using environment: ', buildEnv)
-
-  exec('docker-compose build',
-    {
-      cwd: '.',
-      maxBuffer: bufferSize,
-      env: buildEnv,
-    }, function (err, stdout, stderr) {
-      console.log(stdout);
-      console.error(stderr);
-      cb(err);
-    });
-})
-
-/**
- * Docker up ausführen für das angegebene Environment
- */
-gulp.task('docker-up', function (cb) {
-  const env = argv.env ? argv.env : 'development';
-  const detached = argv.d ? argv.d : '';
-
-  let buildEnv = {
-        APP_ENV: env,
-        NODE_ENV: env,
-  }
-  console.log('using environment: ', buildEnv)
-
-  let upSwitch = '';
-  if (detached) {
-    upSwitch = '-d';
-  }
-
-  exec('docker-compose up ' + upSwitch,
-    {
-      cwd: '.',
-      maxBuffer: bufferSize,
-      env: buildEnv,
-    }, function (err, stdout, stderr) {
-      console.log(stdout);
-      console.error(stderr);
-      cb(err);
-    });
-})
-//-----------------------------------------------------------------------
-// End: Docker
-//-----------------------------------------------------------------------
-
-
-gulp.task('client-tslint', function (cb) {
-  execCommand('gulp tslint', 'client', null, cb);
-})
-
-gulp.task('tslint', ['client-tslint'], () => {
-    return gulp.src([
-      'common/src/**/*.ts',
-      'server/src/**/*.ts',
-      '!**/*.d.ts', '!node_modules/**'
-      ])
-      .pipe(gulp_tslint())
-      .pipe(gulp_tslint.report());
-});
-
-
-gulp.task('update-fluxgate', ['update-fluxgate-common', 'update-fluxgate-client', 'update-fluxgate-server'])
+gulp.task('publish', ['publish-common', 'publish-client', 'publish-server'])
 gulp.task('npm-install', ['install-common', 'install-client', 'install-server'])
-gulp.task('lint', ['lint-server', 'lint-client'])
+gulp.task('lint', ['lint-common', 'lint-server', 'lint-client'])
+
 gulp.task('clean', ['clean-common', 'clean-server', 'clean-client'], function(cb) {
   return del('dist');
 })
+
 gulp.task('really-clean', ['really-clean-common', 'really-clean-server', 'really-clean-client'], function (cb) {
     return del('node_modules');
 })
-gulp.task('build', gulpSequence('info', 'clean', 'build-common', 'build-server', 'build-client', 'deploy'))
-gulp.task('deploy', gulpSequence('deploy-client', 'deploy-common'))
-gulp.task('default', gulpSequence('build'))
 
-//
-// baut alles mit Deployment und startet den Server
-//
-gulp.task('run', gulpSequence('build', 'run-server'))
+gulp.task('update-fluxgate', ['update-fluxgate-client', 'update-fluxgate-server'])
+
+gulp.task('build-all', gulpSequence('build-all-common', ['build-all-server', 'build-all-client']))
+
+gulp.task('build', gulpSequence('info', 'clean', 'build-common', 'build-server', 'build-client', 'publish'))
+
+gulp.task('default', gulpSequence('build'))
