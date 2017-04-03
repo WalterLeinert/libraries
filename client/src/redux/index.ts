6b02f9eb8831061e07d93bc6650a27236b4f9324
Walter Leinert <walter.leinert@outlook.com>
2017-04-03 15:47:23 +0100

    neu: ItemFoundCommand

--- client/src/redux/index.ts
+++ client/src/redux/index.ts
@@ -9,6 +9,7 @@ export * from './finding-items-command';
 export * from './finding-item-by-id-command'
 export * from './item-created-command';
 export * from './item-deleted-command';
+export * from './item-found-command';
 export * from './item-found-by-id-command';
 export * from './item-updated-command';
 export * from './items-found-command';
