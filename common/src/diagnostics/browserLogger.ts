0f4d6e61360bfc1a6a82f5a82dc6f7c16e67161a
Walter Leinert <walter.leinert@outlook.com>
2017-04-03 13:36:34 +0100

    24-StundenFormat

--- common/src/diagnostics/browserLogger.ts
+++ common/src/diagnostics/browserLogger.ts
@@ -14,164 +14,164 @@ import { LoggerRegistry } from './loggerRegistry';
 
 
 export class BrowserLogger implements ILogger {
-    private _level: ILevel = levels.WARN;
+  private _level: ILevel = levels.WARN;
 
-    /**
-     * Konfiguriert das Logging
-     * 
-     * @static
-     * @param {string | IConfig} config
-     * @param {*} [options]
-     * 
-     * @memberOf BrowserLogger    
-     */
-    public static configure(config: string | IConfig, options?: any): void {
+  /**
+   * Konfiguriert das Logging
+   *
+   * @static
+   * @param {string | IConfig} config
+   * @param {*} [options]
+   *
+   * @memberOf BrowserLogger
+   */
+  public static configure(config: string | IConfig, options?: any): void {
 
-        if (typeof config === 'string') {
-            JsonReader.readJson<IConfig>(config, (conf) => {
-                //
-                const cfg = conf as IConfig;
+    if (typeof config === 'string') {
+      JsonReader.readJson<IConfig>(config, (conf) => {
+        //
+        const cfg = conf as IConfig;
 
-                BrowserLogger.applyConfiguration(cfg);
-            });
-        } else {
-            BrowserLogger.applyConfiguration(config);
-        }
+        BrowserLogger.applyConfiguration(cfg);
+      });
+    } else {
+      BrowserLogger.applyConfiguration(config);
     }
+  }
 
 
-    private constructor(private categoryName: string) {
+  private constructor(private categoryName: string) {
+  }
+
+
+  /**
+   * erzeugt eine neue Logger-Instanz
+   */
+  public static create(categoryName: string) {
+    const logger = new BrowserLogger(categoryName);
+    return logger;
+  }
+
+  public trace(message: string, ...args: any[]): void {
+    if (this.isTraceEnabled()) {
+      const sb = this.createLogPrefix(levels.TRACE);
+      sb.append(message);
+      console.trace(sb.toString(), ...args);
     }
+  }
 
-
-    /**
-     * erzeugt eine neue Logger-Instanz
-     */
-    public static create(categoryName: string) {
-        const logger = new BrowserLogger(categoryName);
-        return logger;
+  public debug(message: string, ...args: any[]): void {
+    if (this.isDebugEnabled()) {
+      const sb = this.createLogPrefix(levels.DEBUG);
+      sb.append(message);
+      console.debug(sb.toString(), ...args);
     }
+  }
 
-    public trace(message: string, ...args: any[]): void {
-        if (this.isTraceEnabled()) {
-            const sb = this.createLogPrefix(levels.TRACE);
-            sb.append(message);
-            console.trace(sb.toString(), ...args);
-        }
+  public info(message: string, ...args: any[]): void {
+    if (this.isInfoEnabled()) {
+      const sb = this.createLogPrefix(levels.INFO);
+      sb.append(message);
+      console.info(sb.toString(), ...args);
     }
+  }
 
-    public debug(message: string, ...args: any[]): void {
-        if (this.isDebugEnabled()) {
-            const sb = this.createLogPrefix(levels.DEBUG);
-            sb.append(message);
-            console.debug(sb.toString(), ...args);
-        }
+  public warn(message: string, ...args: any[]): void {
+    if (this.isWarnEnabled()) {
+      const sb = this.createLogPrefix(levels.WARN);
+      sb.append(message);
+      console.warn(sb.toString(), ...args);
     }
+  }
 
-    public info(message: string, ...args: any[]): void {
-        if (this.isInfoEnabled()) {
-            const sb = this.createLogPrefix(levels.INFO);
-            sb.append(message);
-            console.info(sb.toString(), ...args);
-        }
+  public error(message: string, ...args: any[]): void {
+    if (this.isErrorEnabled()) {
+      const sb = this.createLogPrefix(levels.ERROR);
+      sb.append(message);
+      console.error(sb.toString(), ...args);
     }
+  }
 
-    public warn(message: string, ...args: any[]): void {
-        if (this.isWarnEnabled()) {
-            const sb = this.createLogPrefix(levels.WARN);
-            sb.append(message);
-            console.warn(sb.toString(), ...args);
-        }
+  public fatal(message: string, ...args: any[]): void {
+    if (this.isFatalEnabled()) {
+      const sb = this.createLogPrefix(levels.FATAL);
+      sb.append(message);
+      console.error(sb.toString(), ...args);
     }
+  }
 
-    public error(message: string, ...args: any[]): void {
-        if (this.isErrorEnabled()) {
-            const sb = this.createLogPrefix(levels.ERROR);
-            sb.append(message);
-            console.error(sb.toString(), ...args);
-        }
+  public isLevelEnabled(level: ILevel): boolean {
+    return this.level.isLessThanOrEqualTo(level);
+  }
+
+  public isTraceEnabled(): boolean {
+    return this.level.isLessThanOrEqualTo(levels.TRACE);
+  }
+
+  public isDebugEnabled(): boolean {
+    return this.level.isLessThanOrEqualTo(levels.DEBUG);
+  }
+
+  public isInfoEnabled(): boolean {
+    return this.level.isLessThanOrEqualTo(levels.INFO);
+  }
+
+  public isWarnEnabled(): boolean {
+    return this.level.isLessThanOrEqualTo(levels.WARN);
+  }
+
+  public isErrorEnabled(): boolean {
+    return this.level.isLessThanOrEqualTo(levels.ERROR);
+  }
+
+  public isFatalEnabled(): boolean {
+    return this.level.isLessThanOrEqualTo(levels.FATAL);
+  }
+
+
+  public setLevel(level: string | ILevel) {
+    let lev: ILevel;
+
+    if (!Types.isString(level)) {
+      lev = level as ILevel;
+    } else {
+      throw new InvalidOperationException(`Level string ${level} currently not supported`);
     }
+    this._level = lev;
+  }
 
-    public fatal(message: string, ...args: any[]): void {
-        if (this.isFatalEnabled()) {
-            const sb = this.createLogPrefix(levels.FATAL);
-            sb.append(message);
-            console.error(sb.toString(), ...args);
-        }
-    }
+  public get level(): ILevel {
+    return this._level;
+  }
 
-    public isLevelEnabled(level: ILevel): boolean {
-        return this.level.isLessThanOrEqualTo(level);
-    }
+  private createLogPrefix(level: ILevel): StringBuilder {
+    const sb = new StringBuilder(moment().format('YYYY-MM-DD HH:mm:ss.SSS'));
 
-    public isTraceEnabled(): boolean {
-        return this.level.isLessThanOrEqualTo(levels.TRACE);
-    }
+    sb.append(' [');
+    sb.append(level.toString());
+    sb.append('] ');
+    sb.append(this.categoryName);
+    sb.append(' ');
 
-    public isDebugEnabled(): boolean {
-        return this.level.isLessThanOrEqualTo(levels.DEBUG);
-    }
+    return sb;
+  }
 
-    public isInfoEnabled(): boolean {
-        return this.level.isLessThanOrEqualTo(levels.INFO);
-    }
-
-    public isWarnEnabled(): boolean {
-        return this.level.isLessThanOrEqualTo(levels.WARN);
-    }
-
-    public isErrorEnabled(): boolean {
-        return this.level.isLessThanOrEqualTo(levels.ERROR);
-    }
-
-    public isFatalEnabled(): boolean {
-        return this.level.isLessThanOrEqualTo(levels.FATAL);
-    }
-
-
-    public setLevel(level: string | ILevel) {
-        let lev: ILevel;
-
-        if (!Types.isString(level)) {
-            lev = level as ILevel;
-        } else {
-            throw new InvalidOperationException(`Level string ${level} currently not supported`);
-        }
-        this._level = lev;
-    }
-
-    public get level(): ILevel {
-        return this._level;
-    }
-
-    private createLogPrefix(level: ILevel): StringBuilder {
-        const sb = new StringBuilder(moment().format('YYYY-MM-DD hh:mm:ss.SSS'));
-
-        sb.append(' [');
-        sb.append(level.toString());
-        sb.append('] ');
-        sb.append(this.categoryName);
-        sb.append(' ');
-
-        return sb;
-    }
-
-    private static applyConfiguration(config: IConfig) {
-        Object.keys(config.levels).forEach((key) => {
-            if (key.toLowerCase() === '[all]') {
-                const level = Level.toLevel(config.levels[key]);
-                LoggerRegistry.forEachLogger((logger) => {
-                    logger.setLevel(level);
-                });
-
-            } else {
-                const level = Level.toLevel(config.levels[key]);
-                if (LoggerRegistry.hasLogger(key)) {
-                    const logger = LoggerRegistry.getLogger(key);
-                    logger.setLevel(level);
-                }
-            }
+  private static applyConfiguration(config: IConfig) {
+    Object.keys(config.levels).forEach((key) => {
+      if (key.toLowerCase() === '[all]') {
+        const level = Level.toLevel(config.levels[key]);
+        LoggerRegistry.forEachLogger((logger) => {
+          logger.setLevel(level);
         });
-    }
+
+      } else {
+        const level = Level.toLevel(config.levels[key]);
+        if (LoggerRegistry.hasLogger(key)) {
+          const logger = LoggerRegistry.getLogger(key);
+          logger.setLevel(level);
+        }
+      }
+    });
+  }
 
 }
