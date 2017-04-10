d668c997cfd77a594b7e11621d231ef026afe107
Walter Leinert <walter.leinert@outlook.com>
2017-04-10 14:01:28 +0100

    Logging-Konfiguration ergänzt

--- client/test/test.ts
+++ client/test/test.ts
@@ -13,15 +13,35 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+
+
+// -------------------------------------- logging --------------------------------------------
+// tslint:disable-next-line:no-unused-variable
+import { getLogger, ILogger, levels, using, XLog } from '@fluxgate/common';
+// -------------------------------------- logging --------------------------------------------
+
+// Fluxgate
+import { configure, IAppConfig } from '@fluxgate/common';
+
+
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
 declare var require: any;
 
 // Prevent Karma from running prematurely.
 // tslint:disable-next-line:only-arrow-functions
-__karma__.loaded = function() {
+__karma__.loaded = function () {
   // ok
 };
+
+
+
+// tslint:disable-next-line:no-var-requires
+const appConfig: IAppConfig = require('./app/config/config.json');
+
+configure(appConfig.logging);
+
+
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
