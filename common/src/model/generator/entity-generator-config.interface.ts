2f97f7e0eb6b73df0d5ba02fdae9d2f5ddecaf9e
Walter Leinert <walter.leinert@outlook.com>
2017-04-10 15:04:59 +0100

    Kommentare

--- common/src/model/generator/entity-generator-config.interface.ts
+++ common/src/model/generator/entity-generator-config.interface.ts
@@ -1,13 +1,56 @@
 import { TableMetadata } from '../metadata/tableMetadata';
 import { IValueGenerator } from './value-generator.interface';
 
+
+/**
+ * Interface für die Konfiguration des Entitygenerators
+ *
+ * @export
+ * @interface IEntityGeneratorConfig
+ */
 export interface IEntityGeneratorConfig {
+  /**
+   * Anzahl der initial zu erzeugenden Items
+   *
+   * @type {number}
+   * @memberOf IEntityGeneratorConfig
+   */
   count: number;
+
+  /**
+   * Anzahl der maximal erzeugbaren Items
+   *
+   * @type {number}
+   * @memberOf IEntityGeneratorConfig
+   */
   maxCount?: number;
+
+  /**
+   * Generator zum Erzeugen von Entity-Ids
+   *
+   * @type {IValueGenerator}
+   * @memberOf IEntityGeneratorConfig
+   */
   idGenerator: IValueGenerator;
+
+  /**
+   * Metdaten der Tabelle/Entity
+   *
+   * @type {TableMetadata}
+   * @memberOf IEntityGeneratorConfig
+   */
   tableMetadata: TableMetadata;
 
+  /**
+   * Spaltenkonfiguration
+   */
   columns?: {
+
+    /**
+     * Dictionary:
+     * key: Propertyname
+     * value: ein spezieller Value-Generator
+     */
     [key: string]: IValueGenerator
   };
 }
