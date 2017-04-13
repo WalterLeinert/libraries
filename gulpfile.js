1eef768d7e88042efb9f2739ef738b0e7f65a23c
Walter Leinert <walter.leinert@outlook.com>
2017-04-13 09:44:27 +0100

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -130,101 +130,101 @@ gulp.task('docker-up', function (cb) {
 /**
  * Common build
  */
-gulp.task('lint-common', function (cb) {
+gulp.task('tslint:common', function (cb) {
   execCommand('gulp tslint', 'common', bufferSize, cb);
 })
 
-gulp.task('really-clean-common', ['clean-common'], function (cb) {
+gulp.task('really-clean:common', ['clean:common'], function (cb) {
     return del('common/node_modules');
 })
 
-gulp.task('clean-common', function (cb) {
+gulp.task('clean:common', function (cb) {
   execCommand('gulp clean', 'common', bufferSize, cb);
 })
 
-gulp.task('build-common', function (cb) {
+gulp.task('build:common', function (cb) {
   execCommand('gulp', 'common', bufferSize, cb);
 })
 
-gulp.task('test-common', function (cb) {
+gulp.task('test:common', function (cb) {
   execCommand('gulp test', 'common', bufferSize, cb);
 })
 
-gulp.task('publish-common', function (cb) {
+gulp.task('publish:common', function (cb) {
   execCommand('gulp publish -f', 'common', bufferSize, cb);
 })
 
-gulp.task('build-all-common', gulpSequence('clean-common', 'build-common', 'test-common', 'publish-common'))
+gulp.task('build-all:common', gulpSequence('clean:common', 'build:common', 'test:common', 'publish:common'))
 
 
 
 /**
  * Server build
  */
-gulp.task('update-fluxgate-server', function (cb) {
- execCommand('gulp update-fluxgate-common', 'server', bufferSize, cb);
+gulp.task('update-fluxgate:server', function (cb) {
+ execCommand('gulp update-fluxgate:common', 'server', bufferSize, cb);
 })
 
-gulp.task('lint-server', function (cb) {
+gulp.task('tslint:server', function (cb) {
   execCommand('gulp tslint', 'server', bufferSize, cb);
 })
 
-gulp.task('really-clean-server', ['clean-server'], function (cb) {
+gulp.task('really-clean:server', ['clean:server'], function (cb) {
     return del('server/node_modules');
 })
 
-gulp.task('clean-server', function (cb) {
+gulp.task('clean:server', function (cb) {
   execCommand('gulp clean', 'server', bufferSize, cb);
 })
 
-gulp.task('build-server', function (cb) {
+gulp.task('build:server', function (cb) {
   execCommand('gulp', 'server', bufferSize, cb);
 })
 
-gulp.task('test-server', function (cb) {
+gulp.task('test:server', function (cb) {
   execCommand('gulp test', 'server', bufferSize, cb);
 })
 
-gulp.task('publish-server', function (cb) {
+gulp.task('publish:server', function (cb) {
   execCommand('gulp publish -f', 'server', bufferSize, cb);
 })
 
-gulp.task('build-all-server', gulpSequence('clean-server', 'update-fluxgate-server', 'build-server', /*'test-server',*/ 'publish-server'))
+gulp.task('build-all:server', gulpSequence('clean:server', 'update-fluxgate:server', 'build:server', /*'test:server',*/ 'publish:server'))
 
 
 
 /**
  * Client Tasks
  */
-gulp.task('update-fluxgate-client', function (cb) {
- execCommand('gulp update-fluxgate-common', 'client', bufferSize, cb);
+gulp.task('update-fluxgate:client', function (cb) {
+ execCommand('gulp update-fluxgate:common', 'client', bufferSize, cb);
 })
 
-gulp.task('lint-client', function (cb) {
+gulp.task('tslint:client', function (cb) {
   execCommand('gulp tslint', 'client', bufferSize, cb);
 })
 
-gulp.task('really-clean-client', ['clean-client'], function (cb) {
+gulp.task('really-clean:client', ['clean:client'], function (cb) {
     return del('client/node_modules');
 })
 
-gulp.task('clean-client', function (cb) {
+gulp.task('clean:client', function (cb) {
   execCommand('gulp clean', 'client', bufferSize, cb);
 })
 
-gulp.task('test-client', function (cb) {
+gulp.task('test:client', function (cb) {
   execCommand('gulp test', 'client', bufferSize, cb);
 })
 
-gulp.task('build-client', function (cb) {
+gulp.task('build:client', function (cb) {
   execCommand('gulp', 'client', bufferSize, cb);
 })
 
-gulp.task('publish-client', function (cb) {
+gulp.task('publish:client', function (cb) {
   execCommand('gulp publish -f', 'client', bufferSize, cb);
 })
 
-gulp.task('build-all-client', gulpSequence('clean-client', 'update-fluxgate-client', 'build-client', 'test-client', 'publish-client'))
+gulp.task('build-all:client', gulpSequence('clean:client', 'update-fluxgate:client', 'build:client', 'test:client', 'publish:client'))
 
 
 
@@ -232,15 +232,15 @@ gulp.task('build-all-client', gulpSequence('clean-client', 'update-fluxgate-clie
  * initiale Installation mittels npm
  */
 
-gulp.task('install-common', function (cb) {
+gulp.task('install:common', function (cb) {
   execCommand('npm install', 'common', bufferSize, cb);
 })
 
-gulp.task('install-client', function (cb) {
+gulp.task('install:client', function (cb) {
   execCommand('npm install', 'client', bufferSize, cb);
 })
 
-gulp.task('install-server', function (cb) {
+gulp.task('install:server', function (cb) {
   execCommand('npm install', 'server', bufferSize, cb);
 })
 
@@ -254,22 +254,22 @@ gulp.task('info', function () {
 })
 
 
-gulp.task('publish', ['publish-common', 'publish-client', 'publish-server'])
-gulp.task('npm-install', ['install-common', 'install-client', 'install-server'])
-gulp.task('lint', ['lint-common', 'lint-server', 'lint-client'])
+gulp.task('publish', ['publish:common', 'publish:client', 'publish:server'])
+gulp.task('npm-install', ['install:common', 'install:client', 'install:server'])
+gulp.task('tslint', ['tslint:common', 'tslint:server', 'tslint:client'])
 
-gulp.task('clean', ['clean-common', 'clean-server', 'clean-client'], function(cb) {
+gulp.task('clean', ['clean:common', 'clean:server', 'clean:client'], function(cb) {
   return del('dist');
 })
 
-gulp.task('really-clean', ['really-clean-common', 'really-clean-server', 'really-clean-client'], function (cb) {
+gulp.task('really-clean', ['really-clean:common', 'really-clean:server', 'really-clean:client'], function (cb) {
     return del('node_modules');
 })
 
-gulp.task('update-fluxgate', ['update-fluxgate-client', 'update-fluxgate-server'])
+gulp.task('update-fluxgate', ['update-fluxgate:client', 'update-fluxgate:server'])
 
-gulp.task('build-all', gulpSequence('build-all-common', ['build-all-server', 'build-all-client']))
+gulp.task('build-all', gulpSequence('build-all:common', ['build-all:server', 'build-all:client']))
 
-// gulp.task('build', gulpSequence('info', 'clean', 'build-common', 'build-server', 'build-client', 'publish'))
+// gulp.task('build', gulpSequence('info', 'clean', 'build:common', 'build:server', 'build:client', 'publish'))
 
 gulp.task('default', gulpSequence('build-all'))
