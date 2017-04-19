6b21668bb9e147d4d93009195b4748ff08a19665
Walter Leinert <walter.leinert@outlook.com>
2017-04-19 15:47:00 +0100

    ILevel ergänzt

--- platform/src/diagnostics/index.ts
+++ platform/src/diagnostics/index.ts
@@ -1,2 +1,2 @@
-export { using, IConfig, ILogger, levels, Level, Logger, XLog } from '@fluxgate/core';
+export { using, IConfig, ILevel, ILogger, levels, Level, Logger, XLog } from '@fluxgate/core';
 export { configure, getLogger } from './logger';
