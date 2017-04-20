c11251d2bb560b58e4082709c23e368781acb171
Walter Leinert <walter.leinert@outlook.com>
2017-04-20 10:57:46 +0100

    refactorings, weitere Tests

---
+++ common/test/redux/redux-base-test.spec.ts
@@ -0,0 +1,88 @@
+// tslint:disable:member-access
+
+import { Subscription } from 'rxjs/Subscription';
+
+
+// -------------------------- logging -------------------------------
+// tslint:disable-next-line:no-unused-variable
+import { getLogger, ILogger, levels, using, XLog } from '@fluxgate/platform';
+// -------------------------- logging -------------------------------
+
+import { Assert, CustomSubject, ICtor } from '@fluxgate/core';
+
+import { IEntity } from '../../src/model';
+import { IService } from '../../src/model/service/service.interface';
+import { ServiceCommand } from '../../src/redux/service-command';
+import { IServiceState } from '../../src/redux/service-state.interface';
+import { Store } from '../../src/redux/store';
+
+import { ServiceRequests } from '../../src/redux';
+import { CommonTest } from '../common.spec';
+
+
+export class ReduxBaseTest<T extends IEntity<TId>, TId, TService extends IService<T, TId>> extends CommonTest {
+  protected static readonly logger = getLogger(ReduxBaseTest);
+
+  private store: Store;
+  private _serviceRequests: ServiceRequests<T, TId, TService>;
+  private subscriptions: Subscription[] = [];
+  private _commands: Array<ServiceCommand<T, TId>> = [];
+  private _states: Array<IServiceState<T, TId>> = [];
+
+  protected constructor(private storeId: string, private serviceRequestClazz: ICtor<ServiceRequests<T, TId, TService>>,
+    private serviceClazz: ICtor<TService>) {
+    super();
+  }
+
+
+  protected before() {
+    this.store = new Store();
+    this._serviceRequests = new this.serviceRequestClazz(new this.serviceClazz(), this.store);
+
+    this.subscribeToStore(this.storeId);
+    this._commands = [];
+    this._states = [];
+  }
+
+
+  protected subscribeToStore<T, TId>(storeId: string): Subscription {
+    const subscription = this.getStoreSubject(storeId).subscribe((command) => {
+      this.onStoreUpdated(command);
+    });
+    this.subscriptions.push(subscription);
+    return subscription;
+  }
+
+  protected onStoreUpdated(command: ServiceCommand<T, TId>): void {
+    Assert.notNull(command);
+
+    using(new XLog(ReduxBaseTest.logger, levels.INFO, 'onStoreUpdated', `class: ${this.constructor.name}`), (log) => {
+      log.log(`command = ${command.constructor.name}: ${JSON.stringify(command)}`);
+
+      this._commands.push(command);
+      const state = this.getStoreState(command.storeId);
+      this._states.push(state);
+    });
+  }
+
+  protected getStoreState(storeId: string): IServiceState<T, TId> {
+    return this.store.getState<IServiceState<T, TId>>(storeId);
+  }
+
+  protected get serviceRequests(): ServiceRequests<T, TId, TService> {
+    return this._serviceRequests;
+  }
+
+  protected get commands(): Array<ServiceCommand<T, TId>> {
+    return this._commands;
+  }
+
+  protected get states(): Array<IServiceState<T, TId>> {
+    return this._states;
+  }
+
+  private getStoreSubject(storeId: string): CustomSubject<any> {
+    return this.store.subject(storeId);
+  }
+
+}
--- common/test/redux/test.spec.ts
+++ common/test/redux/test.spec.ts
@@ -8,106 +8,77 @@ require('reflect-metadata');
 import { expect } from 'chai';
 import { only, suite, test } from 'mocha-typescript';
 
-import { Subscription } from 'rxjs/Subscription';
 
 // -------------------------- logging -------------------------------
 // tslint:disable-next-line:no-unused-variable
 import { getLogger, ILogger, levels, using, XLog } from '@fluxgate/platform';
 // -------------------------- logging -------------------------------
 
-import { Assert, CustomSubject } from '@fluxgate/core';
 
-import { Client, Column, IEntity, IUser, Table } from '../../src/model';
-import { SetCurrentItemCommand } from '../../src/redux';
-import { ServiceCommand } from '../../src/redux/service-command';
-import { IServiceState } from '../../src/redux/service-state.interface';
-import { Store } from '../../src/redux/store';
+import { IUser } from '../../src/model';
+import { ServiceCommand, ServiceRequestStates } from '../../src/redux';
+import { FindingItemsCommand, ItemsFoundCommand } from '../../src/redux';
+import { FindingItemByIdCommand, ItemFoundByIdCommand } from '../../src/redux';
 
-import { CommonTest } from '../common.spec';
+import { ReduxBaseTest } from './redux-base-test.spec';
 import { UserServiceFake } from './user-service-fake';
 import { UserServiceRequests } from './user-service-requests';
 import { UserStore } from './user-store';
 
 
-@Table({ name: ArtikelRedux.TABLE_NAME })
-class ArtikelRedux implements IEntity<number> {
-  public static readonly TABLE_NAME = 'artikel';
-
-  @Column({ name: 'artikel_id', primary: true, generated: true })
-  public id: number;
-
-  @Column({ name: 'artikel_name', displayName: 'Name' })
-  public name: string;
-
-  @Client()
-  @Column({ name: 'id_mandant' })
-  public id_mandant?: number;
-}
-
-
-
 @suite('redux') @only
-class ReduxTest extends CommonTest {
+class ReduxTest extends ReduxBaseTest<IUser, number, any> {
   protected static readonly logger = getLogger(ReduxTest);
 
-  private store: Store;
-  private serviceRequests: UserServiceRequests;
-  private subscriptions: Subscription[] = [];
-
-  @test 'should exist store and serviceRequests'() {
-    expect(this.store).to.be.not.null;
-    expect(this.serviceRequests).to.be.not.null;
+  constructor() {
+    super(UserStore.ID, UserServiceRequests, UserServiceFake);
   }
 
-  @test 'should call find'() {
+
+  @test 'should call find (FindingItemsCommand, ItemsFoundCommand)'() {
     this.serviceRequests.find();
-  }
 
+    expect(this.commands.length).to.equal(2);
+    expect(this.commands[0]).to.be.instanceOf(FindingItemsCommand);
 
-  protected before() {
-    this.store = new Store();
-    this.serviceRequests = new UserServiceRequests(new UserServiceFake(), this.store);
-    this.subscribeToStore(UserStore.ID);
-  }
-
-  protected subscribeToStore<T, TId>(storeId: string): Subscription {
-    const subscription = this.getStoreSubject(storeId).subscribe((command) => {
-      this.onStoreUpdated(command);
+    const state0 = this.states[0];
+    expect(state0).to.deep.equal({
+      ...ServiceCommand.INITIAL_STATE,
+      state: ServiceRequestStates.RUNNING
     });
-    this.subscriptions.push(subscription);
-    return subscription;
-  }
 
-  protected onStoreUpdated<T extends IEntity<TId>, TId>(command: ServiceCommand<T, TId>): void {
-    Assert.notNull(command);
+    expect(this.commands[1]).to.be.instanceOf(ItemsFoundCommand);
 
-    using(new XLog(ReduxTest.logger, levels.INFO, 'onStoreUpdated', `class: ${this.constructor.name}`), (log) => {
-      log.log(`command = ${command.constructor.name}: ${JSON.stringify(command)}`);
-
-      const state = this.getStoreState(command.storeId);
-      if (state.error) {
-        log.error(`${state.error}`);
-      }
-
-      if (command.storeId === UserStore.ID && command instanceof SetCurrentItemCommand) {
-        this.updateUserState(command);
-      }
+    const state1 = this.states[1];
+    expect(state1).to.deep.equal({
+      ...ServiceCommand.INITIAL_STATE,
+      items: state1.items,
+      state: ServiceRequestStates.DONE
     });
+
   }
 
-  protected getStoreState<T extends IEntity<TId>, TId>(storeId: string): IServiceState<T, TId> {
-    return this.store.getState<IServiceState<T, TId>>(storeId);
-  }
+  @test 'should call findById (FindingItemByIdCommand, ItemFoundCommand)'() {
+    this.serviceRequests.findById(1);
 
-  private updateUserState(command?: ServiceCommand<IUser, number>) {
-    if (command instanceof SetCurrentItemCommand) {
-      // this.currentUserChanged.emit(this.getCurrentUser());
-    }
-  }
+    expect(this.commands.length).to.equal(2);
+    expect(this.commands[0]).to.be.instanceOf(FindingItemByIdCommand);
 
+    const state0 = this.states[0];
+    expect(state0).to.deep.equal({
+      ...ServiceCommand.INITIAL_STATE,
+      state: ServiceRequestStates.RUNNING
+    });
 
-  private getStoreSubject(storeId: string): CustomSubject<any> {
-    return this.store.subject(storeId);
+    expect(this.commands[1]).to.be.instanceOf(ItemFoundByIdCommand);
+
+    const state1 = this.states[1];
+    expect(state1).to.deep.equal({
+      ...ServiceCommand.INITIAL_STATE,
+      item: state1.item,
+      state: ServiceRequestStates.DONE
+    });
+
   }
 
 }
