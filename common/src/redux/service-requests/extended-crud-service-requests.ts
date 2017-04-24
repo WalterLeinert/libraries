45d6eb4d0cbdf8e4880824c8233392e366d8387f
Walter Leinert <walter.leinert@outlook.com>
2017-04-24 13:04:25 +0100

    Umstellung auf CrudServiceRequests-Basisklasse

--- common/src/redux/service-requests/extended-crud-service-requests.ts
+++ common/src/redux/service-requests/extended-crud-service-requests.ts
@@ -2,17 +2,12 @@ import { IToString } from '@fluxgate/core';
 import { IEntity } from '../../model/entity.interface';
 import { IService } from '../../model/service/service.interface';
 import { Store } from '../store/store';
-import { ICommand, ItemDeletedCommand, ItemUpdatedCommand } from './../command/';
-import { ICrudServiceState } from './../state/crud-service-state.interface';
+import { ICommand, ItemDeletedCommand, ItemUpdatedCommand, SetCurrentItemCommand } from './../command/';
 import { ICurrentItemServiceState } from './../state/current-item-service-state.interface';
 import { IExtendedCrudServiceState } from './../state/extended-crud-service-state.interface';
 import { IServiceState } from './../state/service-state.interface';
 import { CrudServiceRequests } from './crud-service-requests';
-import { ICrudServiceRequests } from './crud-service-requests.interface';
-import { CurrentItemServiceRequests } from './current-item-service-requests';
-import { ICurrentItemServiceRequests } from './current-item-service-requests.interface';
 import { IExtendedCrudServiceRequests } from './extended-crud-service-requests.interface';
-import { ServiceRequests } from './service-requests';
 
 
 /**
@@ -25,7 +20,7 @@ import { ServiceRequests } from './service-requests';
  * @template TService
  */
 export abstract class ExtendedCrudServiceRequests<T extends IEntity<TId>, TId extends IToString,
-  TService extends IService<T, TId>> extends ServiceRequests
+  TService extends IService<T, TId>> extends CrudServiceRequests<T, TId, TService>
   implements IExtendedCrudServiceRequests<T, TId> {
 
   public static readonly INITIAL_STATE: IExtendedCrudServiceState<any, any> = {
@@ -33,57 +28,17 @@ export abstract class ExtendedCrudServiceRequests<T extends IEntity<TId>, TId ex
     currentItem: null
   };
 
-  private crudServiceRequests: ICrudServiceRequests<T, TId>;
-  private currentItemServiceRequests: ICurrentItemServiceRequests<T, TId>;
-
   protected constructor(storeId: string, service: TService, store: Store) {
-    super(storeId, store);
+    super(storeId, service, store);
 
-    this.crudServiceRequests = new CrudServiceRequests<T, TId, TService>(storeId, service, store);
-    this.currentItemServiceRequests = new CurrentItemServiceRequests<T, TId>(storeId, store);
   }
-
 
   public setCurrent(item: T): void {
-    this.currentItemServiceRequests.setCurrent(item);
-  }
-
-  public create(item: T): void {
-    this.crudServiceRequests.create(item);
-  }
-
-
-  public find(useCache: boolean = false): void {
-    this.crudServiceRequests.find(useCache);
-  }
-
-  public findById(id: TId): void {
-    this.crudServiceRequests.findById(id);
-  }
-
-  public update(item: T): void {
-    this.crudServiceRequests.update(item);
-  }
-
-  public delete(id: TId): void {
-    this.crudServiceRequests.delete(id);
-  }
-
-  public getEntityId(item: T): TId {
-    return this.crudServiceRequests.getEntityId(item);
-  }
-
-  public getModelClassName(): string {
-    return this.crudServiceRequests.getModelClassName();
-  }
-
-
-  public getCrudState(storeId: string): ICrudServiceState<T, TId> {
-    return this.crudServiceRequests.getCrudState(storeId) as ICrudServiceState<T, TId>;
+    this.dispatch(new SetCurrentItemCommand(this, item));
   }
 
   public getCurrentItemState(storeId: string): ICurrentItemServiceState<T, TId> {
-    return this.currentItemServiceRequests.getCurrentItemState(storeId);
+    return super.getStoreState(storeId) as ICurrentItemServiceState<T, TId>;
   }
 
 
