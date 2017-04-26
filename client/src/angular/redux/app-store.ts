c7177f6a271b1a75e0e87efa2adcf9f3f9dd5545
Walter Leinert <walter.leinert@outlook.com>
2017-04-26 17:21:33 +0100

    APP_STORE in Stringkonstante (wegen aot Problemen)

--- client/src/angular/redux/app-store.ts
+++ client/src/angular/redux/app-store.ts
@@ -7,19 +7,23 @@ import { Store } from '@fluxgate/common';
  * Token für den anwendungsweiten Command-Store
  */
 // export const AppStore = new InjectionToken<Store>('App.store');
+//
+// export const STORE_PROVIDER = {
+//   provide: AppStore,
+//   useFactory: createStore
+// };
 
-export const APP_STORE = {
-  token: 'App.store'
-};
+// TODO: wegen anuglar aot Fehlern in der Anwendung:
+// ERROR in Error encountered resolving symbol values statically.Only initialized variables and constants can be
+// referenced because the value of this variable is needed by the template compiler
+// (position 5:22 in the original .ts file), resolving symbol APP_STORE ...
+
+export const APP_STORE = 'token/fluxgate.client.app-store';
 
 export function createStore(): Store {
   return new Store();
 }
 
-// export const STORE_PROVIDER = {
-//   provide: AppStore,
-//   useFactory: createStore
-// };
 
 export const APP_STORE_PROVIDER = {
   provide: APP_STORE,
