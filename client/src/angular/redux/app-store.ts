a37fa1ab4d369d53224aaac1c9bb9699b6166832
Walter Leinert <walter.leinert@outlook.com>
2017-04-26 10:29:11 +0100

    STORE_PROVIDER ergänzt

--- client/src/angular/redux/app-store.ts
+++ client/src/angular/redux/app-store.ts
@@ -7,3 +7,12 @@ import { Store } from '@fluxgate/common';
  * Token für den anwendungsweiten Command-Store
  */
 export const AppStore = new InjectionToken<Store>('App.store');
+
+export function createStore(): Store {
+  return new Store();
+}
+
+export const STORE_PROVIDER = {
+  provide: AppStore,
+  useFactory: createStore
+};
