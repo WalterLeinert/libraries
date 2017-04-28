5a49c67387c3428adb718ffa9aa44a5cf55171d1
Walter Leinert <walter.leinert@outlook.com>
2017-04-28 09:06:34 +0100

    Deprecated Decorator + Test

---
+++ core/src/decorator/deprecated.ts
@@ -0,0 +1,66 @@
+import { StringBuilder } from '../base/stringBuilder';
+// tslint:disable-next-line:no-unused-variable
+import { ILogger } from '../diagnostics/logger.interface';
+import { getLogger } from '../diagnostics/logging-core';
+import { Types } from '../types/types';
+import { Utility } from '../util/utility';
+
+const logger = getLogger('Deprecated');
+
+/**
+ * Decorator: markiert Klasse, Funktion, Property als deprecated und gibt eine Warnung aus.
+ *
+ * @export
+ * @param {string} [message] - deprecation Message
+ * @param {string} [printStack] - falls true, wird der Stack um die Verwendungsstelle ausgegeben
+ * @returns
+ */
+export function Deprecated(message?: string, printStack: boolean = true) {
+  // tslint:disable-next-line:only-arrow-functions
+  return function (target: any, propertyKey?: string, descriptor?: PropertyDescriptor) {
+    const err = new Error('deprecated');
+    const sb = new StringBuilder('*** ');
+
+    let func;
+    if (Types.isFunction(target)) {
+      // Klassendecorator
+      func = target as Function;
+    } else if (Types.isObject(target)) {
+      // Memberdecorator
+      func = target.constructor as Function;
+    }
+    if (Types.isPresent(func)) {
+      sb.append(func.name);
+    }
+
+    if (!Utility.isNullOrEmpty(propertyKey)) {
+      if (!sb.isEmpty) {
+        sb.append('.');
+      }
+      sb.append(propertyKey);
+    }
+    if (!Utility.isNullOrEmpty(message)) {
+      if (!sb.isEmpty) {
+        sb.append(': ');
+      }
+      sb.append(message);
+    }
+
+    if (printStack) {
+      sb.appendLine();
+
+      // "Error: deprecated" entfernen
+      const stackArray = err.stack.split('\n').slice(1);
+      const length = Math.min(stackArray.length, 5);
+      const stackReduced = stackArray.slice(2, length);
+
+      sb.appendLine(stackReduced.join('\n'));
+      if (length < stackArray.length) {
+        sb.appendLine('    ...');
+      }
+    }
+
+
+    logger.warn(sb.toString());
+  };
+}
