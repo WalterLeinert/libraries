b8b7fa2ae5466273f7934b38b9f5fadf0b88a884
Walter Leinert <walter.leinert@outlook.com>
2017-04-29 14:53:04 +0100

    PromiseExecutor ergänzt

--- core/src/base/objectType.ts
+++ core/src/base/objectType.ts
@@ -9,3 +9,8 @@ export abstract class Funktion {
  * Object vom Typ T (mit Constructor) oder @see{Function}
  */
 export type ObjectType<T> = ICtor<T> | Funktion;
+
+/**
+ * Signatur eines Promise-Executors mit resolve und reject.
+ */
+export type PromiseExecutor<T> = (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void;
