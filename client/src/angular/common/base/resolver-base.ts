d1656efe4ccb6c25161b2a20ecb4508b4c5cae65
Walter Leinert <walter.leinert@outlook.com>
2017-05-04 17:15:41 +0100

    Umstellung auf ServiceRequests

--- client/src/angular/common/base/resolver-base.ts
+++ client/src/angular/common/base/resolver-base.ts
@@ -5,18 +5,20 @@ import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs/Observable';
 
 // Fluxgate
-import { IService } from '@fluxgate/common';
+import { ICrudServiceRequests, IEntity } from '@fluxgate/common';
+import { IToString } from '@fluxgate/core';
 
-export abstract class ResolverBase<T, TId> implements Resolve<T> {
+export abstract class ResolverBase<T extends IEntity<TId>, TId extends IToString> implements Resolve<T> {
 
-  protected constructor(private service: IService<T, TId>, private router: Router) {
+  protected constructor(private serviceRequests: ICrudServiceRequests<T, TId>,
+    private router: Router) {
   }
 
   public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<T> {
     // tslint:disable-next-line:no-string-literal
     const id = route.params['id'];
 
-    return this.service.findById(id)
+    return this.serviceRequests.findById(id)
       .catch((error: Error) => {
         this.router.navigate(['/id']);
         return Observable.of(null);   // TODO
