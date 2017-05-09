e1193f6c492dadcdbe9add4f6ea3b32163b7ad09
Walter Leinert <walter.leinert@outlook.com>
2017-05-09 21:10:54 +0100

    neuer Controller

---
+++ server/src/ts-express-decorators-flx/controllers/entity-version-controller.ts
@@ -0,0 +1,58 @@
+import {
+  Authenticated, Controller, Delete, Get,
+  PathParams, Post, Put,
+  Request
+} from 'ts-express-decorators';
+
+// Fluxgate
+import { EntityVersion, ServiceResult } from '@fluxgate/common';
+
+import { EntityVersionService } from '../services/entityVersion.service';
+import { ControllerBase } from './controllerBase';
+
+
+@Controller('/' + EntityVersion.TABLE_NAME)
+export class EntityVersionController extends ControllerBase<EntityVersion, string> {
+  constructor(service: EntityVersionService) {
+    super(service, service.tableName, service.idColumnName);
+  }
+
+  @Authenticated({ role: 'admin' })
+  @Post('/')
+  public create(
+    @Request() request: Express.Request
+    ): Promise<EntityVersion> {
+    return super.createInternal((request as any).body as EntityVersion);
+  }
+
+  // @Authenticated()
+  @Get('/')
+  public find(
+    ): Promise<EntityVersion[]> {
+    return super.findInternal();
+  }
+
+  // @Authenticated()
+  @Get('/:id')
+  public findById(
+    @PathParams('id') id: string
+    ): Promise<EntityVersion> {
+    return super.findByIdInternal(id);
+  }
+
+  @Authenticated({ role: 'admin' })
+  @Put('/')
+  public update(
+    @Request() request: Express.Request
+    ): Promise<EntityVersion> {
+    return super.updateInternal((request as any).body as EntityVersion);
+  }
+
+  @Authenticated({ role: 'admin' })
+  @Delete('/:id')
+  public delete(
+    @PathParams('id') id: string
+    ): Promise<ServiceResult<string>> {
+    return super.deleteInternal(id);
+  }
+}
