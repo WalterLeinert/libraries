ee15c8605ecef7ff1e1a59b057fb4196aea3c0ed
Christian Lehmeier <clehmeier@fluxgate.de>
2017-06-05 15:15:28 +0200

    Ausbau PrintService

--- server/src/ts-express-decorators-flx/services/print.service.ts
+++ server/src/ts-express-decorators-flx/services/print.service.ts
@@ -2,7 +2,7 @@ import { Service } from 'ts-express-decorators';
 
 // -------------------------- logging -------------------------------
 // tslint:disable-next-line:no-unused-variable
-// import { getLogger, ILogger, levels, using, XLog } from '@fluxgate/platform';
+import { getLogger, ILogger, levels, using, XLog } from '@fluxgate/platform';
 // -------------------------- logging -------------------------------
 
 // Fluxgate
@@ -13,6 +13,17 @@ import { ServiceCore } from './service-core';
 
 @Service()
 export class PrintService extends ServiceCore {
+
+
+  private options = {
+    host: 'localhost',
+    path: '/rest/info?GetPrinters',
+    port: 8881,
+    method: 'POST',
+    json: true,
+    rejectUnauthorized: false,
+    requestCert: true,
+  };
 
 
   /**
@@ -27,20 +38,18 @@ export class PrintService extends ServiceCore {
     request: ISessionRequest
   ): Promise<FindResult<IPrinter>> {
     return new Promise<FindResult<IPrinter>>((resolve) => {
+      const http = require('https');
+      const req = http.get(this.options, (res) => {
+        const bodyChunks = [];
+        res.on('data', (chunk) => {
+          bodyChunks.push(chunk);
+        }).on('end', () => {
+          const body = Buffer.concat(bodyChunks);
+          const printers = JSON.parse(body.toString()) as IPrinter[];
+          resolve(new FindResult(printers, undefined));
 
-      // var req = http.get(options, function (res) {
-      //     var bodyChunks = [];
-      //     res.on('data', function (chunk) {
-      //         bodyChunks.push(chunk);
-      //     }).on('end', function () {
-      //         var body = Buffer.concat(bodyChunks);
-
-      //         myreturn = JSON.parse(body.toString());
-      const printers: IPrinter[] = [];
-      resolve(new FindResult(printers, undefined));
-
-      //     })
-      // });
+        });
+      });
       // const printer = new Printer('pr01');
       // resolve(new FindResult([printer], -1));
     });
