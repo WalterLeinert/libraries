fca3cab5d18c00b33d2003a023d2d7b1cd888958
Christian Lehmeier <clehmeier@fluxgate.de>
2017-06-05 09:44:28 +0200

    Korrektur DB-Patches

--- database/patches/001/001_03_createTableConfig.sql
+++ database/patches/001/001_03_createTableConfig.sql
@@ -8,12 +8,12 @@
 -- Tabellenstruktur für Tabelle `config`
 --
 
-DROP TABLE IF EXISTS `config`;
+DROP TABLE IF EXISTS `systemconfig`;
 
-CREATE TABLE IF NOT EXISTS `config` (
-`config_id` int(11) NOT NULL,
-  `config_name` varchar(50) NOT NULL,
-  `config_json` text NOT NULL,
+CREATE TABLE IF NOT EXISTS `systemconfig` (
+`systemconfig_id` int(11) NOT NULL,
+  `systemconfig_name` varchar(50) NOT NULL,
+  `systemconfig_json` text NOT NULL,
   `__client` int(11) NOT NULL DEFAULT '1',
   `__version` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `config` (
 -- Daten für Tabelle `config`
 --
 
-INSERT INTO `config` (`config_id`, `config_name`, `config_json`, `__client`, `__version`) VALUES
+INSERT INTO `systemconfig` (`systemconfig_id`, `systemconfig_name`, `systemconfig_json`, `__client`, `__version`) VALUES
 (1, 'smtp', '  "mail": {\r\n    "host": "smtp.1und1.de",\r\n    "port": 465,\r\n    "ssl": true,\r\n    "user": "smtp.nodejs@fluxgate.de",\r\n    "password": "Ds43q-Vb5AKpu98-Gbnd",\r\n    "from": "smtp.nodejs@fluxgate.de"\r\n  }', 1, 0);
 
 --
@@ -32,8 +32,8 @@ INSERT INTO `config` (`config_id`, `config_name`, `config_json`, `__client`, `__
 --
 -- Indizes für die Tabelle `config`
 --
-ALTER TABLE `config`
- ADD PRIMARY KEY (`config_id`);
+ALTER TABLE `systemconfig`
+ ADD PRIMARY KEY (`systemconfig_id`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
@@ -42,8 +42,8 @@ ALTER TABLE `config`
 --
 -- AUTO_INCREMENT für Tabelle `config`
 --
-ALTER TABLE `config`
-MODIFY `config_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `systemconfig`
+MODIFY `systemconfig_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- database/patches/001/001_04_updateEntityversion.sql
+++ database/patches/001/001_04_updateEntityversion.sql
@@ -1,1 +1,1 @@
-INSERT INTO `starter`.`entityversion` (`entityversion_id`, `__version`) VALUES ('config', '0');
+INSERT INTO `starter`.`entityversion` (`entityversion_id`, `__version`) VALUES ('systemconfig', '0');
