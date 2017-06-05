16c777ffe4e53d8e6994441e889b3f21bddb9369
Christian Lehmeier <clehmeier@fluxgate.de>
2017-06-05 09:49:26 +0200

    SystemConfig Controller

--- server/src/ts-express-decorators-flx/controllers/index.ts
+++ server/src/ts-express-decorators-flx/controllers/index.ts
@@ -2,5 +2,6 @@ export * from './base/';
 export * from './entity-version-controller';
 export * from './passport-controller';
 export * from './role-controller';
+export * from './systemconfig-controller';
 export * from './user-controller';
 export * from './print-controller';
