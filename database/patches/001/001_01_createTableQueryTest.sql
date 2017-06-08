53563987cc9d3b9a650f9fc90b5f26c802b2cb35
Walter Leinert <walter.leinert@outlook.com>
2017-06-08 09:01:36 +0100

    Fix für Test

--- database/patches/001/001_01_createTableQueryTest.sql
+++ database/patches/001/001_01_createTableQueryTest.sql
@@ -8,3 +8,10 @@ CREATE TABLE `querytest` (
   `__version` INT(11) NOT NULL,
   `test` INT(11),
   PRIMARY KEY (`id`));
+
+  INSERT INTO `querytest` (`id`, `name`, `__status`, `__client`, `__version`, `test`) VALUES
+  ('1', 'None',             '0', '1', '0', '0'),
+  ('2', 'Archived',         '1', '1', '0', '0'),
+  ('3', 'Deleted',          '2', '1', '0', '0'),
+  ('4', 'Archived+Deleted', '3', '1', '0', '0'),
+  ('5', 'Archived 2',       '1', '1', '0', '0');
