694c9a78c87c9b40496497ddc19db09129a557c6
Walter Leinert <walter.leinert@outlook.com>
2017-07-06 16:08:51 +0100

    ci: Fixes für build

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,28 +1,58 @@
 #!/bin/bash
 
-ts=$(date +"%Y-%m-%d-%H-%M-%S")
 
-mkdir -p logs
-logfile="logs/build-${ts}.log"
+# Usage info
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-c] [-o]
+Control the rebuild.
+
+    -c          do a really clean before the rebuild
+    -o          write the result to stdout instead of writing to an logfile.
+EOF
+}
+
+really_clean=0
+write_logfile=1
+
+while getopts "co" opt; do
+  case $opt in
+    c)
+      really_clean=1
+      ;;
+    o)
+      write_logfile=0
+      ;;
+    *)
+      show_help >&2
+            exit 1
+      ;;
+  esac
+done
+shift "$((OPTIND-1))"   # Discard the options and sentinel --
+
+if (($write_logfile)); then
+  ts=$(date +"%Y-%m-%d-%H-%M-%S")
+
+  mkdir -p logs
+  logfile="logs/build-${ts}.log"
+
+  log_redirection="tee logs/build-${ts}.log 2>&1"
+else
+  logfile="-stdout-"
+  log_redirection="tee"
+fi
 
 
 echo "running rebuild, logging to ${logfile}"
 
-
 {
 	set -x
 
-	if [[ "$1" == "--clean" ]]; then
-    shift
-		gulp really-clean
-	fi
 
-  if [[ "$1" == "--no-publish" ]]; then
-    shift
-		publish_cmd=""
-  else
-  	publish_cmd="publish -f"
-	fi
+  if (($really_clean)); then
+      gulp really-clean
+  fi
 
 	npm install
 
@@ -33,6 +63,6 @@ echo "running rebuild, logging to ${logfile}"
 		gulp ${publish_cmd}
 	)
 	done
-} > logs/build-${ts}.log 2>&1
+} | ${log_redirection}
 
 # tail -f logs/build-${ts}.log
