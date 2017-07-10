3d5a0d18c7138bc1fbced87144144f20fce3a04b
Walter Leinert <walter.leinert@outlook.com>
2017-07-10 08:31:21 +0100

    obsolete imports entfernt

--- core/test/di/module/module-test.ts
+++ core/test/di/module/module-test.ts
@@ -9,14 +9,6 @@ import { Injectable, InjectionToken, ReflectiveInjector } from 'injection-js';
 import { expect } from 'chai';
 import { suite, test } from 'mocha-typescript';
 
-import { CoreInjector } from '../../../src/di/core-injector';
-import { ConsoleLogger } from '../../../src/diagnostics/consoleLogger';
-import { DEFAULT_CATEGORY, LOG_EXCEPTIONS, LOGGER } from '../../../src/diagnostics/logger.token';
-import { SimpleStringifyer } from '../../../src/diagnostics/simple-stringifyer';
-import { STRINGIFYER } from '../../../src/diagnostics/stringifyer.token';
-
-
-
 import { Component } from '../../../src/di/component.decorator';
 import { ModuleMetadataStorage } from '../../../src/di/module-metadata-storage';
 import { Module } from '../../../src/di/module.decorator';
