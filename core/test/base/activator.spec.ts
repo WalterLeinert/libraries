6c21b14d400dd4645d10f57b215268f86c859bcf
Walter Leinert <walter.leinert@outlook.com>
2017-07-12 12:39:38 +0100

    Formatierung

--- core/test/base/activator.spec.ts
+++ core/test/base/activator.spec.ts
@@ -4,40 +4,41 @@
 import { expect } from 'chai';
 import { suite, test } from 'mocha-typescript';
 
-import { Activator } from '../../src/base/activator';import { CoreUnitTest } from '../unit-test';
+import { Activator } from '../../src/base/activator';
+import { CoreUnitTest } from '../unit-test';
 
 class Test {
-    constructor(public name: string, public id: number) {
-    }
+  constructor(public name: string, public id: number) {
+  }
 }
 
 
 class Test2 {
-    constructor() {
-        // ok
-    }
+  constructor() {
+    // ok
+  }
 }
 
 @suite('core.base.Activator')
 class ActivatorTest extends CoreUnitTest {
 
-    @test 'should create instance of class Test'() {
-        const expectedTest = new Test('hugo', 4711);
-        return expect(Activator.createInstance<Test>(Test, expectedTest.name, expectedTest.id)).to.be.eql(expectedTest);
-    }
+  @test 'should create instance of class Test'() {
+    const expectedTest = new Test('hugo', 4711);
+    return expect(Activator.createInstance<Test>(Test, expectedTest.name, expectedTest.id)).to.be.eql(expectedTest);
+  }
 
 
-    @test 'should create instance of class Test: 2nd parameter missing'() {
-        const expectedTest = new Test('hugo', undefined);
-        return expect(Activator.createInstance<Test>(Test, expectedTest.name)).to.be.eql(expectedTest);
-    }
+  @test 'should create instance of class Test: 2nd parameter missing'() {
+    const expectedTest = new Test('hugo', undefined);
+    return expect(Activator.createInstance<Test>(Test, expectedTest.name)).to.be.eql(expectedTest);
+  }
 
-    @test 'should create instance of class Test: all parameters missing'() {
-        const expectedTest = new Test(undefined, undefined);
-        return expect(Activator.createInstance<Test>(Test)).to.be.eql(expectedTest);
-    }
+  @test 'should create instance of class Test: all parameters missing'() {
+    const expectedTest = new Test(undefined, undefined);
+    return expect(Activator.createInstance<Test>(Test)).to.be.eql(expectedTest);
+  }
 
-    @test 'should create instance of class Test2'() {
-        return expect(Activator.createInstance<Test2>(Test2)).to.be.not.null;
-    }
+  @test 'should create instance of class Test2'() {
+    return expect(Activator.createInstance<Test2>(Test2)).to.be.not.null;
+  }
 }
