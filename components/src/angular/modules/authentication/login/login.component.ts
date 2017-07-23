e0080bbfca52430ef2a5f66f54470c58cc056297
Walter Leinert <walter.leinert@outlook.com>
2017-07-23 10:29:52 +0100

    Fix für Validierung

--- components/src/angular/modules/authentication/login/login.component.ts
+++ components/src/angular/modules/authentication/login/login.component.ts
@@ -12,7 +12,8 @@ import { getLogger, ILogger, levels, using, XLog } from '@fluxgate/platform';
 // -------------------------- logging -------------------------------
 
 
-import { ExtendedCoreComponent, MessageService } from '@fluxgate/client';
+import { ExtendedCoreComponent, FormGroupInfo, MessageService, MetadataService } from '@fluxgate/client';
+import { IUser, User } from '@fluxgate/common';
 import { Core } from '@fluxgate/core';
 
 // commands
@@ -31,7 +32,11 @@ import { PassportService } from '../passport.service';
       <label class="col-form-label col-sm-2" for="username">Name</label>
       <div class="col-sm-5">
         <input flxAutofocus type="text" class="form-control" formControlName="username" id="username" required
-          [(ngModel)]="username" placeholder="Username">
+          [(ngModel)]="user.username" placeholder="Username">
+      </div>
+
+      <div *ngIf="getFormErrors('username')" class="alert alert-danger">
+        {{ getFormErrors('username') }}
       </div>
     </div>
 
@@ -39,13 +44,17 @@ import { PassportService } from '../passport.service';
       <label class="col-form-label col-sm-2" for="password">Password:</label>
       <div class="col-sm-5">
         <input type="password" class="form-control" formControlName="password" id="password" required
-          [(ngModel)]="password" placeholder="Password">
+          [(ngModel)]="user.password" placeholder="Password">
+      </div>
+
+      <div *ngIf="getFormErrors('password')" class="alert alert-danger">
+        {{ getFormErrors('password') }}
       </div>
     </div>
 
     <div class="form-group row">
       <div class="col-sm-5">
-        <button type="submit" class="btn btn-primary" [disabled]="form.invalid" (click)='login()'>Login</button>
+        <button type="submit" class="btn btn-primary" [disabled]="!isValid()" (click)='login()'>Login</button>
       </div>
     </div>
   </form>
@@ -65,8 +74,7 @@ import { PassportService } from '../passport.service';
 export class LoginComponent extends ExtendedCoreComponent {
   protected static logger = getLogger(LoginComponent);
 
-  public username: string;
-  public password: string;
+  public user: IUser;
 
 
   /**
@@ -84,16 +92,15 @@ export class LoginComponent extends ExtendedCoreComponent {
    */
   constructor(private serviceRequests: CurrentUserServiceRequests,
     private fb: FormBuilder, router: Router, route: ActivatedRoute, messageService: MessageService,
+    private metadataService: MetadataService,
     @Inject(AuthenticationNavigationToken) private authenticationNavigation: AuthenticationNavigation,
     private service: PassportService) {
     super(router, route, messageService);
 
     using(new XLog(LoginComponent.logger, levels.INFO, 'ctor'), (log) => {
-
-      this.buildFormFromValidators(this.fb, {
-        username: ['', Validators.required],
-        password: ['', Validators.required],
-      });
+      this.user = this.buildFormFromModel<User>(this.fb, User, this.metadataService, FormGroupInfo.DEFAULT_NAME, [
+        'username', 'password'
+      ]);
     });
   }
 
@@ -103,7 +110,7 @@ export class LoginComponent extends ExtendedCoreComponent {
       const clientId = 1;     // TODO
       log.warn(`Login component um clientId erweitern`);
 
-      this.registerSubscription(this.service.login(this.username, this.password, clientId)
+      this.registerSubscription(this.service.login(this.user.username, this.user.password, clientId)
         .subscribe((result) => {
           log.log(Core.stringify(result));
 
