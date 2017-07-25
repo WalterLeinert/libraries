264b6c71ef4505ac87b30bd86a9bc5b6d9dd40ea
Walter Leinert <walter.leinert@outlook.com>
2017-07-25 11:07:35 +0100

    obsoleten QueryController entfernt

--- server/src/ts-express-decorators-flx/controllers/base/index.ts
+++ server/src/ts-express-decorators-flx/controllers/base/index.ts
@@ -1,6 +1,5 @@
 export * from './controller-base';
 export * from './controller-core';
 export * from './core-controller';
-export * from './query-controller';
 export * from './readonly-controller';
 export * from './table-controller';
