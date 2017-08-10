0ebda5e33b4cecdd0282b04b265582a4ee8a66a5
Christian Lehmeier <clehmeier@fluxgate.de>
2017-08-10 22:25:16 +0200

    Sidebar: Template und Styles in TS überführt

--- components/src/angular/modules/sidebar/sidebar.component.ts
+++ components/src/angular/modules/sidebar/sidebar.component.ts
@@ -5,8 +5,77 @@ import { ResizeEvent } from 'angular-resizable-element';
 
 @Component({
   selector: 'flx-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  template: `
+  <ng - sidebar - container class="noscroll" >
+  <!--A sidebar -- >
+  <ng-sidebar class="ng-sidebar noscroll"[(opened)] = "opened" style= "height:100%;"[mode] = "mode"
+  [closeOnClickOutside] = "closeOnClickOutside"
+  [closeOnClickBackdrop] = "false" dock= true
+  [dockedSize] = "dockedSize"
+  [sidebarClass] = "demo-sidebar" >
+  <div mwlResizable class="noscroll borderright"
+  [ngStyle] = "{'width': menueSize + 'px', 'height':'100%', 'background-color': '#eeeeee'}"
+  [resizeEdges] = "{right: true}"
+  [resizeCursorPrecision] = "10"
+  [enableGhostResize] = "true"(resizeStart) = "onResizeStart($event)"
+    (resizeEnd) = "onResizeEnd($event)" >
+  <div style="width:100%; height:35px; padding:0" >
+  <a class="btn btn-sm pull-right" (click) = "toggleSidebar()" data-toggle="tooltip"
+  data-placement="right" title="toggle sidebar" > <i class="fa fa-bars fa-lg" > </i></a>
+  <a class="btn btn-sm pull-right" (click) = "togglePinned()" data-toggle="tooltip"
+  data-placement="right" title="toggle sidebar pin" > <i[ngClass]="pinbutton" > </i></a>
+  </div>
+
+  < div id= Containerfixed >
+  <ng-content select= "[flx-sidebar-icons]" > </ng-content>
+  < /div>
+
+  < div id= Containerauto >
+  <ng-content select= "[flx-sidebar-main]" > </ng-content>
+  < /div>
+
+  < /div>
+  < /ng-sidebar>
+
+  < !--Page content -- >
+  <div ng- sidebar - content >
+  <ng-content select= "[flx-sidebar-detail]" > </ng-content>
+  < /div>
+  < /ng-sidebar-container>
+  `,
+  styles: [`
+#Containerfixed {
+  width: 40px;
+  float: right;
+}
+
+#Containerauto {
+  overflow-y:scroll;
+  overflow-x:hidden;
+  height:95%;
+  height:calc(100%-50px);
+  height: -moz-calc(100%-50px);
+  height: -webkit-calc(100%-50px);
+}
+
+.noscroll {
+  overflow:hidden;
+}
+
+.borderright {
+    border: 0px solid #cccccc;
+    border-right-width: 1px;
+}
+
+.shadow {
+    box-shadow: 0 0 1.5em rgba(85,85,85,.5);
+}
+
+.ng-sidebar--docked {
+  box-shadow: 0 0 1.5em rgba(85,85,85,.5);
+}
+
+  `]
 })
 export class SidebarComponent implements OnInit {
   public static readonly MIN_MENU_WIDTH = 250;
