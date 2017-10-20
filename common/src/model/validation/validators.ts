26ba7701254c67701e19a32dbed57d42b0512d75
Walter Leinert <walter.leinert@outlook.com>
2017-10-20 09:36:27 +0100

    Kommentare ergänzt

--- common/src/model/validation/validators.ts
+++ common/src/model/validation/validators.ts
@@ -8,16 +8,64 @@ export class Validators {
   private static readonly mailPattern = '[a-z0-9!#$%&\' * +/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_\`{|}~-]+)*@' +
   '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?';
 
+  /**
+   * Validator für gültige Emails.
+   *
+   * @static
+   * @type {IValidation}
+   * @memberof Validators
+   */
   public static email: IValidation = new PatternValidator(Validators.mailPattern, 'Invalid Email');
+
+  /**
+   * Validator für ganze Zahlen
+   *
+   * @static
+   * @type {IValidation}
+   * @memberof Validators
+   */
   public static integer: IValidation = new PatternValidator('^[+-]?[0-9]+', 'Invalid number');
+
+  /**
+   * Validator für ganze positive Zahlen
+   *
+   * @static
+   * @type {IValidation}
+   * @memberof Validators
+   */
   public static positiveInteger: IValidation = new PatternValidator('^[0-9]+', 'Invalid positive number');
 
+  /**
+   * Validator für Werte, die nicht optional sind.
+   *
+   * @static
+   * @type {IValidation}
+   * @memberof Validators
+   */
   public static required: IValidation = new RequiredValidator();
 
+  /**
+   * Validator für einen numerischen Wertebereich
+   *
+   * @static
+   * @param {IRangeOptions} options
+   * @returns {IValidation}
+   * @memberof Validators
+   */
   public static range(options: IRangeOptions): IValidation {
     return new RangeValidator(options);
   }
 
+
+  /**
+   * Validator für Regex-Pattern
+   *
+   * @static
+   * @param {string} pattern
+   * @param {string} [info]
+   * @returns {IValidation}
+   * @memberof Validators
+   */
   public static pattern(pattern: string, info?: string): IValidation {
     return new PatternValidator(pattern, info);
   }
