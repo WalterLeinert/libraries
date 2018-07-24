65c7c9bb508b6388df44f25f240dfbedbcbd94a3
Walter Leinert <walter.leinert@aracom.de>
2018-07-24 08:58:23 +0200

    build(test): removed export bootstrap

--- packages/client/src/public_api.ts
+++ packages/client/src/public_api.ts
@@ -2,4 +2,9 @@
  * Public API Surface of clienttest
  */
 
-export * from './lib/index';
+export * from './lib/angular';
+// export * from './lib/bootstrap';
+export * from './lib/base';
+export * from './lib/primeng';
+
+// export * from './lib/index';
