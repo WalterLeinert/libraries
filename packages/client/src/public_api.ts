444c3e067e1fb113d7612e7be26b63bfdb292d1d
Walter Leinert <walter.leinert@aracom.de>
2018-07-24 15:02:20 +0200

    fix for barrel issue with angular compiler

--- packages/client/src/public_api.ts
+++ packages/client/src/public_api.ts
@@ -2,9 +2,21 @@
  * Public API Surface of clienttest
  */
 
-export * from './lib/angular';
+// does not work for barrels with current angular compiler??
+// export * from './lib/angular/';
 // export * from './lib/bootstrap';
-export * from './lib/base';
-export * from './lib/primeng';
+// export * from './lib/base';
+// export * from './lib/primeng';
 
 // export * from './lib/index';
+
+
+// workaround
+export * from './lib/angular/common/adapter/index';
+export * from './lib/angular/common/base/index';
+export * from './lib/angular/common/routing/index';
+export * from './lib/angular/redux/index';
+export * from './lib/angular/services/index';
+export * from './lib/base/displayConfiguration/index';
+export * from './lib/primeng/adapter/index';
+export * from './lib/primeng/calendarLocale';
