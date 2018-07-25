fdacaba6424deb7effdfa982fc03752915a5b778
Walter Leinert <walter.leinert@aracom.de>
2018-07-25 14:05:10 +0200

    components: moved source to src/lib

--- packages/components/src/public_api.ts
+++ packages/components/src/public_api.ts
@@ -3,26 +3,30 @@
 // })();
 
 // does not work for barrels with current angular compiler??
-export * from './angular';
-export * from './testing';
+// export * from './angular';
+// export * from './testing';
 
 // workaround
-export * from './angular/modules/authentication/index';
-export * from './angular/modules/autoform/index';
-export * from './angular/modules/common/index';
-export * from './angular/modules/datatable-selector/index';
-export * from './angular/modules/directives/index';
-export * from './angular/modules/dropdown-selector/index';
-export * from './angular/modules/enum-value/index';
-export * from './angular/modules/messages/index';
-export * from './angular/modules/month-selector/index';
-export * from './angular/modules/role-selector/index';
-export * from './angular/modules/selection-list/index';
-export * from './angular/modules/sidebar/index';
-export * from './angular/modules/tab/index';
-export * from './angular/modules/time-selector/index';
-export * from './angular/modules/user-selector/index';
-export * from './angular/modules/year-selector/index';
-export * from './angular/redux/index';
-export * from './angular/services/index';
-export * from './testing/index';
+export * from './lib/angular/modules/authentication/index';
+export * from './lib/angular/modules/autoform/index';
+export * from './lib/angular/modules/autoform/autoform-controls/autoform-controls.component';
+export * from './lib/angular/modules/autoform/autoform-controls/autoform-controls.module';
+export * from './lib/angular/modules/common/index';
+export * from './lib/angular/modules/common/confirmation-dialog/confirmation-dialog.component';
+export * from './lib/angular/modules/common/popup/popup.component';
+export * from './lib/angular/modules/datatable-selector/index';
+export * from './lib/angular/modules/directives/index';
+export * from './lib/angular/modules/dropdown-selector/index';
+export * from './lib/angular/modules/enum-value/index';
+export * from './lib/angular/modules/messages/index';
+export * from './lib/angular/modules/month-selector/index';
+export * from './lib/angular/modules/role-selector/index';
+export * from './lib/angular/modules/selection-list/index';
+export * from './lib/angular/modules/sidebar/index';
+export * from './lib/angular/modules/tab/index';
+export * from './lib/angular/modules/time-selector/index';
+export * from './lib/angular/modules/user-selector/index';
+export * from './lib/angular/modules/year-selector/index';
+export * from './lib/angular/redux/index';
+export * from './lib/angular/services/index';
+export * from './lib/testing/index';
