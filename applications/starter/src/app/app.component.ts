8224234a0ddeddb11484ad31a788e3c55699e3c2
Walter Leinert <walter.leinert@aracom.de>
2018-07-25 10:24:30 +0200

    intermediate commit: first fluxgate integration

--- applications/starter/src/app/app.component.ts
+++ applications/starter/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import * as process from 'process';
+
+import { Component, OnInit } from '@angular/core';
+
+import { NotSupportedException } from '@fluxgate/core';
+// -------------------------------------- logging --------------------------------------------
+// tslint:disable-next-line:no-unused-variable
+// import { getLogger, ILogger, levels, using, XLog } from '@fluxgate/platform';
+// -------------------------------------- logging --------------------------------------------
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  // protected static logger = getLogger(AppComponent);
+
+  public title = 'app';
+
+  public years: number[] = [
+  ];
+
+  public selectedYear: number;
+
+  public ngOnInit(): void {
+    // return using(new XLog(AppComponent.logger, levels.INFO, 'ngOnInit'), (log) => {
+    throw new NotSupportedException('test for module import');
+    // });
+  }
 }
--- applications/starter/src/app/app.module.ts
+++ applications/starter/src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// import { YearSelectorModule } from '@fluxgate/components';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // YearSelectorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
