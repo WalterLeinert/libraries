ebbb43578e7bc6560c34479504934605edc97dfb
Walter Leinert <walter.leinert@hp.com>
2018-07-26 11:32:52 +0200

    made export conditional

--- packages/platform/src/lib/diagnostics/index.ts
+++ packages/platform/src/lib/diagnostics/index.ts
@@ -1,3 +1,5 @@
 export { using, IConfig, ILevel, ILogger, levels, Level, XLog } from '@fluxgate/core';
 export { configure, getLogger } from './logging-platform';
+// removeIf(browser)
 export { ILoggingConfigurationOptions, LoggingConfiguration } from './loggingConfiguration';
+// endRemoveIf(browser)
