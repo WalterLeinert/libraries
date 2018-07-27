075828b247f5fe3f2ffdd1cad3d9c7bfc77d7c6d
Walter Leinert <walter.leinert@hp.com>
2018-07-27 10:53:34 +0200

    added missing providers, module

--- applications/starter/src/app/app.module.ts
+++ applications/starter/src/app/app.module.ts
@@ -2,14 +2,17 @@ import { Injector, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import { ConfirmationService } from 'primeng/components/common/api';
 
 // -------------------------------------- logging --------------------------------------------
 // tslint:disable-next-line:no-unused-variable
 import { configure, getLogger, ILogger, levels, using, XLog } from '@fluxgate/platform';
 // -------------------------------------- logging --------------------------------------------
 
-import { APP_STORE, AppInjector, createStore } from '@fluxgate/client';
+import { APP_STORE, AppInjector, createStore, CurrentUserService } from '@fluxgate/client';
 import { AppConfig, IAppConfig } from '@fluxgate/common';
 import { ENTITY_VERSION_SERVICE_PROVIDER, YearSelectorModule } from '@fluxgate/components';
 import { ModuleMetadataStorage } from '@fluxgate/core';
@@ -17,6 +20,7 @@ import { ModuleMetadataStorage } from '@fluxgate/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientModule } from './client.module';
+
 
 
 // import * as appConfig from './config/config.json';
@@ -27,6 +31,7 @@ import { ClientModule } from './client.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
 
@@ -35,6 +40,8 @@ import { ClientModule } from './client.module';
     AppRoutingModule
   ],
   providers: [
+    ConfirmationService,
+    CurrentUserService,
     ENTITY_VERSION_SERVICE_PROVIDER,
     { provide: APP_STORE, useFactory: createStore },
   ],
